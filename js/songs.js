aa300ee451f5ed81bc87dc426474b07764741bb5
Daniel Moore <hi@danielmoore.us>
2019-11-03 11:17:07 -0800

    Sun Nov  3 11:17:07 Pacific Standard Time 2019

--- js/songs.js
+++ js/songs.js
@@ -2270,7 +2270,7 @@ const songs = [
 "Foo Fighters|This Is a Call|1995|h-Rnr3wTX9I|Foo Fighters|S78",
 "Everclear|Santa Monica (Watch the World Die)|1995|MW6E_TNgCsY|Everclear (band)|S2124",
 "Donna Summer|Bad Girls|1979|NF46NnUn5nw|Donna Summer|S577",
-"Air|Le Soleil est près de moi|1997|ItidwkXHr5g|Air (band)|S2137",
+"Air|Le soleil est près de moi|1997|ItidwkXHr5g|Air (band)|S2137",
 "Deep Purple|Child in Time|1970|PfAWReBmxEs|Deep Purple|S6016",
 "Luther Vandross|Never Too Much|1981|uv7y6PKEYms|Luther Vandross|S7354",
 "Deerhunter|Desire Lines|2010|aW2jOA6uIaM|Deerhunter|S4898",
