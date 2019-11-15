2456a2feb774a286f442f579d914f6142e51deb5
Daniel Moore <hi@danielmoore.us>
2019-11-15 11:58:40 -0800

    Fri Nov 15 11:58:40 Pacific Standard Time 2019

--- js/songs.js
+++ js/songs.js
@@ -8748,7 +8748,7 @@ const songs = [
 "Nicolas Jaar|Space Is Only Noise If You Can See|2011|1f2Ov14_rKw|Nicolas Jaar|S7739",
 "Nasty Habits|Shadow Boxing|1996|z_VKMWs2eaI|https://www.discogs.com/artist/1640-Nasty-Habits|S12730",
 "Alan Vega|Jukebox Babe|1980|OAQ2jq6vwsE|Alan Vega|S5141",
-"Neon Indian|Polish Girl|2011|Fo6ZaCTqxwM|Neon Indian|S7697",
+"Neon Indian|Polish Girl|2011|cuPaP6xbQsM?start=39|Neon Indian|S7697",
 "Hervé Vilard|Capri c'est fini|1965|4TP3u0rNkys|Hervé Vilard|S11032",
 "Hop Along|Waitress|2015|AU39gpBHMK4?start=7|Hop Along|S9777",
 "Interpol|Rest My Chemistry|2007|C4BXD_oK284|Interpol (band)|S6793",
