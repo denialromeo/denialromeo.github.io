0c56026357ae3ec05465477ded401ae89f5caa5a
Daniel Moore <hi@danielmoore.us>
2019-12-22 17:21:21 -0800

    $(date)

--- bin/mp42mp4.bat
+++ bin/mp42mp4.bat
@@ -1,4 +1,1 @@
-@python -c "import sys;print(sys.argv[1][0:-4])" %1 > temp.txt
-@set /p filename=<temp.txt
-@del temp.txt
-@ffmpeg -i %1 -strict -2 "%filename%_.mp4"
+@ffmpeg -i "%~1" -strict -2 "%~n1_.mp4"
--- bin/subtitle.bat
+++ bin/subtitle.bat
@@ -1,9 +1,3 @@
-@python -c "import sys;print(sys.argv[1][0:-4])" %1 > temp.txt
-@python -c "import sys;print(sys.argv[1][0:-4])" %2 > temp1.txt
-@set /p filename0=<temp.txt
-@set /p filename1=<temp1.txt
-@del temp.txt
-@del temp1.txt
-@ffmpeg -i %2 "%filename1%.ass"
-@ffmpeg -i %1 -vf ass="%filename1%.ass" "%filename0%_.mp4"
-@del "%filename1%.ass"
+@ffmpeg -i "%~2" "%~n2.ass"
+@ffmpeg -i "%~1" -vf ass="%~n2.ass" "%~n1_.mp4"
+@del "%~n2.ass"
