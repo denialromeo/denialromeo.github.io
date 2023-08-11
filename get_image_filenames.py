51b093ffdda4bfe9d09399d26810e79795368829
Daniel Moore <hi@danielmoore.us>
2023-08-10 19:41:59 -0700

    Commit.

--- get_image_filenames.py
+++ get_image_filenames.py
@@ -1,11 +1,11 @@
 import re
 
-file_path = '_includes/author.html'  # Update this path to your file
+file_path = 'about.md'  # Update this path to your file
 
-# Regular expression pattern to find full filenames ending with .jpg, .jpeg, or .png
-pattern = r'\b\w+\.(?:jpg|jpeg|png)\b'
+# Regular expression pattern to find full filenames, including parent directories, ending with .jpg, .jpeg, or .png
+pattern = r'\b[\w/]+\.(?:jpg|jpeg|png)\b'
 
-# Open the file and read the content
+# Open the file and read the content using UTF-8 encoding
 with open(file_path, 'r', encoding='utf-8') as file:
     content = file.read()
     # Find all occurrences of the pattern in the content
