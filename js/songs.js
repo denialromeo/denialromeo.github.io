b084c7cfa804b864ba735a21a23b4cb280264290
Daniel Moore <hi@danielmoore.us>
2023-08-31 19:38:21 -0700

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -4714,7 +4714,7 @@ const songs = [
 "Lykke Li|No Rest for the Wicked|2014|Hh-0y8Qe0Sw|Lykke Li|S9572|Art Pop;Indie Pop;Pop|No Rest for the Wicked (song)",
 "Jackie Wilson|Baby Workout|1963|7p0HaD0aQmM|Jackie Wilson|S3966|Rhythm & Blues;Soul;R&B|Baby Workout",
 "Yardbirds|Happenings Ten Years Time Ago|1966|OJZHnZrUoQ0|The Yardbirds|S9083|Freakbeat;Psychedelic Rock;Beat Music;Pop;Pop Rock;Rock|Happenings Ten Years Time Ago",
-"Louis Prima|Just a Gigolo/I Ain't Got Nobody|1956|Kkrb4h4weW4|Louis Prima|S7332|Big Band;Swing;Traditional Pop;Jazz;Pop|I Ain't Got Nobody",
+"Louis Prima|Just a Gigolo/I Ain't Got Nobody|1956|Kkrb4h4weW4|Louis Prima|S7332|Big Band;Swing;Traditional Pop;Jazz;Pop|https://en.wikipedia.org/wiki/I_Ain't_Got_Nobody#%22Just_a_Gigolo/I_Ain't_Got_Nobody%22_medley",
 "Paul Simon|Still Crazy After All These Years|1975|Q5Eoax6I-O4|Paul Simon|S11867|Pop;Singer/Songwriter|",
 "Etta James|I Just Want to Make Love to You|1961|5t2DQs09qeo|Etta James|S4388|Rhythm & Blues;Soul;R&B|I Just Want to Make Love to You",
 "Destiny's Child|Bills, Bills, Bills|1999|NiF6-0UTqtc|Destiny's Child|S2376|Contemporary R&B;Pop;R&B|Bills, Bills, Bills",
