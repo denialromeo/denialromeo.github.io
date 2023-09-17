22289d30f81e28854c973ef09ee2ca021e61e413
Daniel Moore <hi@danielmoore.us>
2023-09-17 11:01:02 -0700

    Commit.

--- js/classical.js
+++ js/classical.js
@@ -89,7 +89,7 @@ const classical = [
 "Antonio Vivaldi|The Four Seasons|1725|GRxofEmo3HA|Antonio Vivaldi|The Four Seasons (Vivaldi)",
 "Johann Sebastian Bach|Six Violin Sonatas|1725|s7wmR0EqEGQ|Johann Sebastian Bach|",
 "Johann Sebastian Bach|Keyboard Partitas|1726-1731|XiG8AGn5Qz8|Johann Sebastian Bach|",
-"Johann Sebastian Bach|Motets|1726-1735|jdtYTR6zP8A|Johann Sebastian Bach|",
+"Johann Sebastian Bach|Motets|1726-1735|jdtYTR6zP8A|Johann Sebastian Bach|List of motets by Johann Sebastian Bach",
 "Johann Sebastian Bach|St. Matthew Passion|1727|ZwVW1ttVhuQ|Johann Sebastian Bach|",
 "George Frideric Handel|Coronation Anthems|1727|oXmNnF9v8mU|George Frideric Handel|",
 "John Gay|The Beggar’s Opera|1728|Lq7g_NRKHU0|John Gay|The Beggar's Opera",
