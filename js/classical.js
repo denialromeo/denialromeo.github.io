0964f0e66e29ddf7f9c21a27e24d898a6b741448
Daniel Moore <hi@danielmoore.us>
2023-10-12 18:28:24 -0700

    Commit.

--- js/classical.js
+++ js/classical.js
@@ -214,7 +214,7 @@ const classical = [
 "Franz Schubert|Lieder|1814-1828|fYiw37hpZtY|Franz Schubert|",
 "Ludwig van Beethoven|Cello Sonatas, Op. 102|1815|nUFGQqzznkM|Ludwig van Beethoven|Cello Sonatas Nos. 4 and 5 (Beethoven)",
 "Nicolò Paganini|Violin Concertos nos. 1-6|1815-1830|7AfxA17L8bg|Niccolò Paganini|Violin Concerto No. 1 (Paganini)",
-"Gioachino Rossini|The Barber of Seville|1816|OloXRhesab0|Gioachino Rossini|The Barber of Seville",
+"Gioachino Rossini|The Barber of Seville|1816|gDbBhzIcYj4|Gioachino Rossini|The Barber of Seville",
 "Franz Schubert|Symphony No. 5|1816|OHkot1TmvZU|Franz Schubert|Symphony No. 5 (Schubert)",
 "Nicolò Paganini|Twenty-four Caprices|1817|WMLoBXgPil8|Niccolò Paganini|24 Caprices for Solo Violin (Paganini)",
 "Franz Schubert|Violin Sonata in A major|1817|to1IAyAv0iI|Franz Schubert|",
