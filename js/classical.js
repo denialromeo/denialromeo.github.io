8410333748658d5d96f2fbe3bc63d89ab69a9f4d
Daniel Moore <hi@danielmoore.us>
2023-10-29 14:12:22 -0700

    Commit.

--- js/classical.js
+++ js/classical.js
@@ -40,7 +40,7 @@ const classical = [
 "Heinrich Schütz|Musikalische Exequien|1636|ikNYP6H7Ilw|Heinrich Schütz|Musikalische Exequien",
 "Gregorio Allegri|Miserere mei Deus|1640s|H3v9unphfi0|Gregorio Allegri|Miserere (Allegri)",
 "Claudio Monteverdi|Il ritorno d’Ulisse in patria|1640|zunvtJBkS9E|Claudio Monteverdi|Il ritorno d'Ulisse in patria",
-"Claudio Monteverdi|L’incoronazione di Poppea|1643|rzXEYAdPJ84|Claudio Monteverdi|",
+"Claudio Monteverdi|L’incoronazione di Poppea|1643|rzXEYAdPJ84|Claudio Monteverdi|L'incoronazione di Poppea",
 "Giacomo Carissimi|Jephte|1650|_TN9VI1vNKM|Giacomo Carissimi|Jephte (Carissimi)",
 "Barbara Strozzi|Cantatas|1650s|BWZoYpDLDX0|Barbara Strozzi|",
 "Francesco Cavalli|La Calisto|1651|vZToJPUxEz4?start=56|Francesco Cavalli|La Calisto",
