9a53f009f15a5861534a5bcf290a6fecb402c4f3
Daniel Moore <hi@danielmoore.us>
2023-11-06 10:48:40 -0800

    Commit.

--- js/albums.js
+++ js/albums.js
@@ -1562,7 +1562,7 @@ const albums = [
 "Kris Kristofferson|Kristofferson/Me and Bobby McGee|1970|PL0A4746B94BEAE0E0|A2341|Kristofferson (album)|Kris Kristofferson|Outlaw Country;Singer/Songwriter;Country;Progressive Country",
 "Kanye West|The Life of Pablo|2016|OLAK5uy_keyJA-JsoAdfCylvyyIQMRjenzX6MzmnE|A5754|The Life of Pablo|Kanye West|Pop Rap;Hip Hop",
 "Dexys Midnight Runners|Don't Stand Me Down|1985|OLAK5uy_njvflOFHDDdjNAuCo2vpVfurlAlmSSsVY|A638|Don't Stand Me Down|Dexys Midnight Runners|Blue Eyed Soul;New Wave;Pop;Rock",
-"Willie Colón & Rubén Blades|Siembra|1978|OLAK5uy_lTi6fYtkv70bmelxgm5mfgv9oxbo5hKdo|A2737|Siembra|Rubén Blades|Salsa Dura;Hispanic Music;Latin American Music;Regional Music;Salsa",
+"Willie Colón & Rubén Blades|Siembra|1978|OLAK5uy_k_fDM8eajRPOHa4n4UWt75ktogXzrsPVg|A2737|Siembra|Rubén Blades|Salsa Dura;Hispanic Music;Latin American Music;Regional Music;Salsa",
 "Oscar Peterson|Night Train|1963|PLL-NbN8uTOihRe6lBaChjGgQ9oWylqiZj|A2319|Night Train (Oscar Peterson album)|Oscar Peterson|Cool Jazz;Jazz",
 "The Hold Steady|Separation Sunday|2005|OLAK5uy_kgpg0oPUHL4On3KzsOugJdOX8wLST10hA|A4175|Separation Sunday|The Hold Steady|Indie Rock;Pub Rock;Alternative Rock;Rock",
 "Jon Hopkins|Immunity|2013|OLAK5uy_lKA2GOHTZI6wSo_W03FKlUDS4s03a0wIo|A5320|Immunity (Jon Hopkins album)|Jon Hopkins|IDM;Microhouse;Dance;Electronic;Electronic Dance Music;House",
