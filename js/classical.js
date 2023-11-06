20e7cd4f495be15466e4da8357b8fc37b13d2966
Daniel Moore <hi@danielmoore.us>
2023-11-05 17:33:02 -0800

    Commit.

--- js/classical.js
+++ js/classical.js
@@ -736,7 +736,7 @@ const classical = [
 "Igor Stravinsky|Oedipus rex|1927|O0vVLj-9y7o|Igor Stravinsky|Oedipus rex (opera)",
 "Zoltán Kodály|Háry János Suite|1927|ym2QvHQNyPU|Zoltán Kodály|Háry János",
 "Eugène Goossens|Oboe Concerto|1927|VyFu8ToIJEQ|Eugene Aynsley Goossens|Oboe Concerto (Vaughan Williams)",
-"Constant Lambert|The Rio Grande|1927|_I1eHvdwdv4|Constant Lambert|",
+"Constant Lambert|The Rio Grande|1927|_I1eHvdwdv4|Constant Lambert|The Rio Grande (Lambert)",
 "Edgard Varèse|Arcana|1927|Bg3-Sdn4PPg|Edgard Varèse|",
 "George Gershwin|An American in Paris|1928|K4I2OzMltM4|George Gershwin|An American in Paris",
 "Maurice Ravel|Boléro|1928|r30D3SW4OVw|Maurice Ravel|Boléro",
