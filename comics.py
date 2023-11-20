da08d85cb7e9995d87f96e3440a125bb4689b431
Daniel Moore <hi@danielmoore.us>
2023-11-20 10:47:36 -0800

    Commit.

--- comics.py
+++ comics.py
@@ -1,41 +1,39 @@
 import json
+import os
 
 from PIL import Image
 
-JSON = 'comics-single.json'
+DIR = 'assets/img/comics/web'
+TITLE = 'The Extinction Parade'
+AUTHOR = 'Max Brooks'
 
 def get_dim(img):
     im = Image.open(img);
     return f"{im.size[0]}x{im.size[1]}"
 
-def first_slide(page):
+def first_slide(img_link, title, author):
     return ('<div class="gallery comic-gallery" itemscope="" itemtype="http://schema.org/ImageGallery">\n'
             '<figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">'
-            '<a href="{img_link}" itemprop="contentUrl" data-size="{dim}">'
-            '<img style="border: 1px solid black;" src="{img_link}"/>'
+            f'<a href="{img_link}" itemprop="contentUrl" data-size="{get_dim(img_link)}">'
+            f'<img style="border: 1px solid black;" src="{img_link}"/>'
             '</a>'
             '<figcaption itemprop="caption description">'
-            '<p><i>{title}</i> by {author}</p>'
-            # '<p><a href="{amazon}">Amazon</a></p>'
+            f'<p><i>{title}</i> by {author}</p>'
             '</figcaption>'
-            '</figure>\n').format(dim=get_dim(page["link"]), img_link='/' + page["link"], **page)
+            '</figure>\n')
 
-def slides(comics_json):
-    s = first_slide(comics_json["pages"][0])
-    caption = '<p><i>{title}</i> by {author}</p>' if 'single' not in JSON else ''
-    for i in range(1, len(comics_json["pages"])):
+def slides(directory, title, author):
+    imgs = [directory + '/' + f for f in os.listdir(directory)]
+    s = first_slide(imgs[0], title, author)
+    for img_link in imgs[1:]:
         s += ('<figure itemprop="associatedMedia" style="display:none" itemscope itemtype="http://schema.org/ImageObject">'
-              '<a href="{img_link}" itemprop="contentUrl" data-size="{dim}">'
-              '<img src="{img_link}"/>'
+              f'<a href="{img_link}" itemprop="contentUrl" data-size="{get_dim(img_link)}">'
+              f'<img src="{img_link}"/>'
               '</a>'
-              f'{caption}'
-              # '<p><a href="{amazon}">Amazon</a></p>'
               '</figcaption>'
-              '</figure>\n').format(img_link='/' + comics_json["pages"][i]["link"], dim=get_dim(comics_json["pages"][i]["link"]), **(comics_json["pages"][i]))
+              '</figure>\n')
     s += '</div>'
     return s
 
 if __name__ == '__main__':
-    with open(JSON) as data_file:
-        comics_json = json.load(data_file)
-        print(slides(comics_json))
+    print(slides(DIR, TITLE, AUTHOR))
