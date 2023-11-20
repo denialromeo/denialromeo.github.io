2668f94b6d65c0782c8b1efd37f1e112dc8adc5c
Daniel Moore <hi@danielmoore.us>
2023-11-19 18:43:17 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -7817,7 +7817,7 @@ const songs = [
 "Noname|Blaxploitation|2018|ebz8I00to0M|Noname (rapper)|S16610|Conscious Hip Hop;Jazz Rap;Hip Hop|",
 "The Jesus and Mary Chain|Sometimes Always|1994|MntPYnT2p3c|The Jesus and Mary Chain|S4501|Indie Rock;Alternative Rock;Rock|Sometimes Always",
 "The Beatles|The Word|1965|RfBEqiEhCgM|The Beatles|S10121|Pop Rock;Pop;Rock|The Word (song)",
-"Johnny Cash|The Mercy Seat|2000|XoJ0bAp4Eo8|Johnny Cash|S6977|Americana;Country|The Mercy Seat (song)",
+"Johnny Cash|The Mercy Seat|2000|Y3411MJO6Y0|Johnny Cash|S6977|Americana;Country|The Mercy Seat (song)",
 "The Jam|The Bitterest Pill|1982|WBXng6QFF1k|The Jam|S3508|Mod Revival;New Wave;Mod;Rock|",
 "Captain Beefheart and His Magic Band|Sure 'Nuff 'n Yes I Do|1967|ayUqmladJuo|Captain Beefheart|S5710|Blues Rock;Garage Rock;Rock|Electricity (Captain Beefheart song)",
 "Dionne Warwick|That's What Friends Are For|1985|HyTpu6BmE88|Dionne Warwick|S6088|Adult Contemporary;Pop Soul;Pop;R&B;Soul|",
