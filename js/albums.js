17b5bc5cdbd22366cbe0a93c6698d77b746c1232
Daniel Moore <hi@danielmoore.us>
2023-11-28 11:09:27 -0800

    Commit.

--- js/albums.js
+++ js/albums.js
@@ -2746,7 +2746,7 @@ const albums = [
 "Mahmoud Ahmed|Eré Méla Méla/Ethiopiques 7|1975|OLAK5uy_mmJt1U1EItVgLiwFZTXFfKvifS6FC9oGE|A4562|Éthiopiques|Mahmoud Ahmed|Ethio-Jazz;Tizita;African Music;Jazz;Regional Music",
 "Robert Fripp|Exposure|1979|PLbnpw-XH5DOFMv-Gx3vNF3Rdsf-4CuCAs|A479|Exposure (Robert Fripp album)|Robert Fripp|Art Rock;Experimental Rock;Rock",
 "John Coltrane|Olé Coltrane|1961|OLAK5uy_nvJEkOWeJu3R9O67ruFWHSlPEiypFo7H4|A2471|Olé Coltrane|John Coltrane|Modal Jazz;Jazz",
-"Metro Area|Metro Area|2002|PLnxSY27MRIcHR8fTi_Q8KEdS1uFTPdvDp|A4951|Metro Area (album)|Metro Area|Electro-Disco;Nu-Disco;Dance;Disco;Electronic;Electronic Dance Music;House",
+"Metro Area|Metro Area|2002|PLNupLsYXugExS9kuYJeeMWOi3qnLAwJlg|A4951|Metro Area (album)|Metro Area|Electro-Disco;Nu-Disco;Dance;Disco;Electronic;Electronic Dance Music;House",
 "Barbra Streisand|The Broadway Album|1985|OLAK5uy_ljaypFLt1XK35aTj0DmBpGOj3vjZxbcAc|A2044|The Barbra Streisand Album|Barbra Streisand|Show Tunes;Standards;Descriptor;Musical Theatre and Entertainment;Pop;Soundtracks;Traditional Pop",
 "Cheap Trick|Heaven Tonight|1978|OLAK5uy_nYhDGl7l_MoMD9urqzJ75pKwYqy_8emKY|A1446|Heaven Tonight|Cheap Trick|Hard Rock;Power Pop;Pop;Pop Rock;Rock",
 "Big Daddy Kane|Long Live the Kane|1988|OLAK5uy_kik14pu1FnvsRr-y-PcJduXznPEzu9rDE|A3228|Long Live the Kane|Big Daddy Kane|Boom Bap;Hardcore Hip Hop;Hip Hop",
