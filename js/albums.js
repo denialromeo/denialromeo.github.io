6f6bfbee66568d9476bf7bbb2e6cd0c4e24671ec
Daniel Moore <hi@danielmoore.us>
2023-12-08 22:12:22 -0800

    Commit.

--- js/albums.js
+++ js/albums.js
@@ -2464,7 +2464,7 @@ const albums = [
 "John Coltrane|Meditations|1966|OLAK5uy_krwq5cKtkCVdD3rrDjvwbGHqNU_zeqEqI|A1966|Meditations (John Coltrane album)|John Coltrane|Free Jazz;Avant-Garde Jazz;Jazz",
 "Bee Gees|Odessa|1969|OLAK5uy_m34VO7iYGr_MnC2JSRLZgFq2vfPS4fbBo|A4475|Odessa (Bee Gees album)|Bee Gees|Baroque Pop;Pop Rock;Pop;Rock",
 "Jim O'Rourke|Insignificance|2001|PLGxyU_3RFNSQDdvZJBS5ViGjLtxsJMNX6|A4942|Insignificance (Jim O'Rourke album)|Jim O'Rourke (musician)|Alternative Singer/Songwriter;Indie Rock;Alternative Rock;Rock;Singer/Songwriter",
-"Jimmy Page & Robert Plant|No Quarter - Unledded|1994|OLAK5uy_l9Bs5lsxRqCuJBcNHiUe0rmaQrPBpl1AU|A4806|No Quarter: Jimmy Page and Robert Plant Unledded|Page and Plant|Acoustic Rock;Blues Rock;Rock",
+"Jimmy Page & Robert Plant|No Quarter - Unledded|1994|PL0F8Cv3jNP6UMIWPW4i_WSX8ExzIMmJyv|A4806|No Quarter: Jimmy Page and Robert Plant Unledded|Page and Plant|Acoustic Rock;Blues Rock;Rock",
 "Laura Nyro and Labelle|Gonna Take a Miracle|1971|OLAK5uy_nDpvnNKO5hGGV63Z9NmZL0s7lXNLRj3QU|A1344|Gonna Take a Miracle|Labelle|Pop Soul;Rhythm & Blues;Pop;R&B;Soul",
 "Clap Your Hands Say Yeah|Clap Your Hands Say Yeah|2005|OLAK5uy_mE8oXIHUMJOuB11JRSjGwH8hfbXKrartw|A4204|Clap Your Hands Say Yeah (album)|Clap Your Hands Say Yeah|Indie Rock;Alternative Rock;Rock",
 "Fear Factory|Demanufacture|1995|OLAK5uy_mWvX4onNane-9D9Ke4owvpQrqC0Kx6d6A|A3575|Demanufacture (album)|Fear Factory|Industrial Metal;Industrial Music;Metal;Rock",
