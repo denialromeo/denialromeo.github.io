68b25f404112cbce471bf8c50ffad10724d32bc2
Daniel Moore <hi@danielmoore.us>
2023-12-22 18:51:21 -0800

    Commit.

--- js/albums.js
+++ js/albums.js
@@ -2153,7 +2153,7 @@ const albums = [
 "Sunn O)))|Monoliths And Dimensions|2009|OLAK5uy_m2ygqqIxFnqZp6o6loXhx3znKpO9q_2po|A5018|Monoliths & Dimensions|Sunn O)))|Drone;Drone Metal;Experimental;Metal;Rock",
 "Ministry|The Land of Rape and Honey|1988|OLAK5uy_mnBmYMT81eN2DyqpfJuBSxusMXPZ9wMDE|A3219|The Land of Rape and Honey|Ministry (band)|Industrial;Industrial Metal;Industrial Rock;Experimental;Industrial Music;Metal;Rock",
 "Louis Prima|The Wildest!|1956|PLowQCq3Ss89ggMNfeLPVgsW0BjuJxPWBj|A2236|The Wildest!|Louis Prima|Big Band;Swing;Traditional Pop;Jazz;Pop",
-"Bryan Adams|Reckless|1984|PLUvfsDHKjxwsq4qT2PAvvHsFr6AeBJTcq|A3012|Reckless (Bryan Adams album)|Bryan Adams|Pop Rock;Pop;Rock",
+"Bryan Adams|Reckless|1984|PLILCJF6YwPP8sOzT-_aeyHs5vg_cCGyjb|A3012|Reckless (Bryan Adams album)|Bryan Adams|Pop Rock;Pop;Rock",
 "The Everly Brothers|Songs Our Daddy Taught Us|1958|OLAK5uy_k8sUJCM8oYsO3-5EKyhJ3BzlVOp-LOyFo|A28|Songs Our Daddy Taught Us|The Everly Brothers|Close Harmony;Country Pop;Folk Pop;American Folk Music;Contemporary Folk;Country;Folk;Pop;Regional Music;Traditional Country;Traditional Folk Music",
 "Harry Belafonte|Belafonte at Carnegie Hall|1959|OLAK5uy_n16sF0YHHJ83OaEFiJS_QXfUl-KB15bb4|A1856|Belafonte at Carnegie Hall|Harry Belafonte|Caribbean Folk Music;Caribbean Music;Folk;Regional Music;Traditional Folk Music",
 "Count Basie|Count Basie Swings, Joe Williams Sings|1955|OLAK5uy_kealVZhQ5oEX_eO_O4TZJ_BgVmKNbz-2k|A1780|Count Basie Swings, Joe Williams Sings|Count Basie|Big Band;Swing;Vocal Jazz;Jazz",
