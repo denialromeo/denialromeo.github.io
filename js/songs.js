641de6a1eccb62b620d67c81908ae720bbc3dab2
Daniel Moore <hi@danielmoore.us>
2023-12-28 13:51:21 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -4193,7 +4193,7 @@ const songs = [
 "2Pac|Keep Ya Head Up|1993|UhqblJCadEA|Tupac Shakur|S4494|Conscious Hip Hop;West Coast Hip Hop;Hip Hop|Keep Ya Head Up",
 "Fucked Up|Queen of Hearts|2011|syg6XGbdUkM|Fucked Up|S4963|Indie Rock;Post-Hardcore;Alternative Rock;Punk Rock;Rock|David Comes to Life|",
 "Paul Hardcastle|19|1985|oho9KaoxI-4|Paul Hardcastle|S7877|Electro;Synthpop;Dance;Electronic;Electronic Dance Music;Pop|19 (song)",
-"Ice-T|6 in the Mornin'|1986|izuMg1GGnMc|Ice-T|S6769|West Coast Hip Hop;Hip Hop|6 in the Mornin'",
+"Ice-T|6 in the Mornin'|1986|pzCGPlDgbgU|Ice-T|S6769|West Coast Hip Hop;Hip Hop|6 in the Mornin'",
 "Death Cab for Cutie|I Will Possess Your Heart|2008|pq-yP7mb8UE|Death Cab for Cutie|S4710|Indie Rock;Alternative Rock;Rock|I Will Possess Your Heart",
 "Tone-Loc|Funky Cold Medina|1989|63ZIf2H9S0E|Tone Loc|S259|Pop Rap;West Coast Hip Hop;Hip Hop|",
 "Earth, Wind & Fire|Fantasy|1977|L0CVoFsUhC4|Earth, Wind & Fire|S6187|Smooth Soul;R&B;Soul|Fantasy (Earth, Wind & Fire song)",
