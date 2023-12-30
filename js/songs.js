7e60b6b14694a8c9535e56b63b9c8430769eec96
Daniel Moore <hi@danielmoore.us>
2023-12-29 22:18:03 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -4024,7 +4024,7 @@ const songs = [
 "Joey Dee|Peppermint Twist|1961|7WIvZu4dPQQ|Joey Dee and the Starliters|S4387|Rhythm & Blues;Rock & Roll;R&B;Rock|Peppermint Twist",
 "Brothers Johnson|Strawberry Letter 23|1977|mXpAzy0WVPg|The Brothers Johnson|S10302|Funk;Soul;R&B|Strawberry Letter 23",
 "Teenage Fanclub|Star Sign|1991|YHfasfkX8MM|Teenage Fanclub|S189|Alternative Rock;Power Pop;Pop;Pop Rock;Rock|",
-"Art Tatum|Tea for Two|1939|kACt0FM0Kf8|Art Tatum|S2749|Stride;American Folk Music;Folk;Jazz;Ragtime;Regional Music;Traditional Folk Music|Tea for Two (song)",
+"Art Tatum|Tea for Two|1939|btJWbMXTx2U|Art Tatum|S2749|Stride;American Folk Music;Folk;Jazz;Ragtime;Regional Music;Traditional Folk Music|Tea for Two (song)",
 "Bing Crosby|I'll Be Seeing You|1944|1ld7pkvZPQk|Bing Crosby|S5467|Traditional Pop;Pop|I'll Be Seeing You (song)",
 "I-F|Space Invaders Are Smoking Grass|1997|tIAZTV_sYtk|I-F|S30|Electro;Electroclash;Dance;Electronic;Electronic Dance Music|",
 "Frank Sinatra|I Get a Kick out of You|1955|wSrHvNr8QQQ?start=5|Frank Sinatra|S9222|Standards;Vocal Jazz;Jazz;Pop;Traditional Pop|I Get a Kick Out of You",
