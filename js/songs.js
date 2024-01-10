a27971765f247bf510fd0befc7a2686a71e4348c
Daniel Moore <hi@danielmoore.us>
2024-01-09 21:04:36 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -8649,7 +8649,7 @@ const songs = [
 "Associated Glee Clubs of America|Adeste Fideles|1925|BuuUyPsLQes|https://www.discogs.com/artist/4643811-Associated-Glee-Clubs-Of-America|S15256|Choral;Christmas Music;Classical Music;Descriptor;Holiday Music;Western Classical Music|",
 "Andrew Hill|Refuge|1965|nPHUz8apHjY|Andrew Hill (jazz musician)|S9992|Avant-Garde Jazz;Jazz|",
 "Superpitcher|Tomorrow|2001|M9n7QLJvWZg|Superpitcher|S8640|Deep House;Microhouse;Dance;Electronic;Electronic Dance Music;House|",
-"Serge Gainsbourg|Variations sur Marilou|1976|0XRL6S7qrx8|Serge Gainsbourg|S8361|Chanson;French Pop;Pop;Regional Music|",
+"Serge Gainsbourg|Variations sur Marilou|1976|mLKK3L1Wp_4|Serge Gainsbourg|S8361|Chanson;French Pop;Pop;Regional Music|",
 "Jack Nitzsche|The Lonely Surfer|1963|1o_5z6-OIPY|Jack Nitzsche|S3669|Baroque Pop;Surf Rock;Pop;Rock;Surf Music|",
 "Nazareth|Hair of the Dog|1975|UVfJo07pNIc|Nazareth (band)|S11722|Hard Rock;Rock|Hair of the Dog (song)",
 "Queens of the Stone Age|Regular John|1998|wb-57OuONjQ|Queens of the Stone Age|S14912|Stoner Rock;Hard Rock;Rock|",
