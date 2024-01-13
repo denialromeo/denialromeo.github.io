53fd313aba463ae5782e9fb6865b3d9c8d429fde
Daniel Moore <hi@danielmoore.us>
2024-01-13 07:23:25 -0800

    Commit.

--- js/classical.js
+++ js/classical.js
@@ -573,7 +573,7 @@ const classical = [
 "Sergei Rachmaninoff|Symphony No. 2|1907|SvuitFzDxDg|Sergei Rachmaninoff|Symphony No. 2 (Rachmaninoff)",
 "Nicolai Rimsky-Korsakov|The Golden Cockerel Suite|1907|484zNF-MpQo|Nikolai Rimsky-Korsakov|",
 "Edward Elgar|Symphony No. 1|1908|6SX9m2V9mbM|Edward Elgar|Symphony No. 1 (Elgar)",
-"Maurice Ravel|Gaspard de la nuit|1908|jJRnNm_jhEs|Maurice Ravel|Gaspard de la nuit",
+"Maurice Ravel|Gaspard de la nuit|1908|f39a8rhk8mI|Maurice Ravel|Gaspard de la nuit",
 "Isaac Albéniz|Iberia|1908|R13GcRQSPxU|Isaac Albéniz|Iberia (Albéniz)",
 "Alexander Scriabin|The Poem of Ecstasy|1908|tOjQ4j9bLvg|Alexander Scriabin|",
 "Maurice Ravel|Rapsodie espagnole|1908|tvwx7IeZky8|Maurice Ravel|Rapsodie espagnole",
