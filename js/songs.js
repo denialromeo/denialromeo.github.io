f8fae4cc43d504d83e173a7ece79b73864392be8
Daniel Moore <hi@danielmoore.us>
2024-01-13 15:39:55 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -3493,7 +3493,7 @@ const songs = [
 "Ben Folds Five|Brick|1997|Wt5EHAqhR1c|Ben Folds Five|S3209|Piano Rock;Singer/Songwriter;Pop;Pop Rock;Rock|Brick (song)",
 "Daryl Hall & John Oates|Sara Smile|1976|dYEpFJhuu1E|Hall & Oates|S6671|Blue Eyed Soul;Pop Rock;Pop;Rock|Sara Smile",
 "The Moody Blues|Go Now!|1964|BEGUVLpTzOs?start=23|The Moody Blues|S3074|Beat Music;Pop;Pop Rock;Rock|Go Now",
-"Shep and the Limelites|Daddy's Home|1961|Y8DICjr5jBI|Shep and the Limelites|S2804|Doo-Wop;Descriptor;R&B;Vocal Group|Daddy's Home (song)",
+"Shep and the Limelites|Daddy's Home|1961|Oz_AfJvIDd8|Shep and the Limelites|S2804|Doo-Wop;Descriptor;R&B;Vocal Group|Daddy's Home (song)",
 "Spencer Davis Group|I'm a Man|1967|POCUgBSVENQ|The Spencer Davis Group|S8522|British Rhythm & Blues;Mod;R&B;Rhythm & Blues;Rock|I'm a Man (The Spencer Davis Group song)",
 "The Rascals|People Got to Be Free|1968|CYcz7gMnifk|The Rascals|S924|Blue Eyed Soul;Pop Rock;Pop;Rock|People Got to Be Free",
 "Bob Marley and the Wailers|Lively Up Yourself|1971|bDnNqsy7rYE|Bob Marley|S1313|Roots Reggae;Caribbean Music;Jamaican Music;Reggae;Regional Music|",
