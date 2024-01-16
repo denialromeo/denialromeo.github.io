0e8c6c08f7008fd92413faf189c9a9fb2d025bf5
Daniel Moore <hi@danielmoore.us>
2024-01-15 19:50:32 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -7650,7 +7650,7 @@ const songs = [
 "The Magnetic Fields|Andrew in Drag|2012|Jf_l3EGQvL8|The Magnetic Fields|S7381|Indie Pop;Pop|",
 "Tricky|Makes Me Wanna Die|1996|XyFIIUKAOE8|Tricky (musician)|S4235|Trip Hop;Downtempo;Electronic|",
 "Led Zeppelin|Nobody's Fault But Mine|1976|9_kqkZTZYrg|Led Zeppelin|S7212|Blues Rock;Hard Rock;Rock|It's Nobody's Fault but Mine",
-"The Coral|Pass It On|2003|zkiz4pq94Eg|The Coral|S2263|Indie Pop;Indie Rock;Alternative Rock;Pop;Rock|Pass It On|",
+"The Coral|Pass It On|2003|17HzPpyKjfc|The Coral|S2263|Indie Pop;Indie Rock;Alternative Rock;Pop;Rock|Pass It On|",
 "Depeche Mode|Somebody|1984|kBP-EaXU1Vc|Depeche Mode|S15417|Art Pop;Singer/Songwriter;Synthpop;Electronic;Pop|",
 "Next|Too Close|1997|kwEZRPkAAu8|Next (American band)|S7718|Contemporary R&B;R&B|Too Close (Next song)",
 "Stan Ridgway|Camouflage|1986|KoKbnUD6gMU|Stan Ridgway|S12293|Country Rock;Pop Rock;Pop;Rock|Camouflage (Stan Ridgway song)",
