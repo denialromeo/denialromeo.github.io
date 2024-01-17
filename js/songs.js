dec7b67cea80cb1223032ba353cdc34136d2c2c0
Daniel Moore <hi@danielmoore.us>
2024-01-16 19:24:34 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -3250,7 +3250,7 @@ const songs = [
 "Nirvana|Sliver|1990|QECJ9pCyhns|Kurt Cobain|S2995|Grunge;Alternative Rock;Rock|Sliver (song)",
 "Sam Smith|Stay With Me|2014|pB-5XG-DbAA|Sam Smith (singer)|S9584|Blue Eyed Soul;Pop Soul;Pop;R&B;Soul|Stay with Me (Sam Smith song)",
 "Charlie Parker|Now's the Time|1946|ryNtmkfeJk4|Charlie Parker|S9184|Bebop;Jazz|",
-"Stray Cats|Runaway Boys|1980|H7mfA48KJ8o?start=4|Stray Cats|S3862|Rockabilly;Rock;Rock & Roll|Stray Cats (album)",
+"Stray Cats|Runaway Boys|1980|HFMdI2D_cSk|Stray Cats|S3862|Rockabilly;Rock;Rock & Roll|Stray Cats (album)",
 "Fat White Family|Feet|2019|avXN2a0WJ5U|Fat White Family|S16771|Alternative Dance;Art Rock;Neo-Psychedelia;Alternative Rock;Dance;Psychedelic Rock;Rock|",
 "Anderson .Paak feat. T.I.|Come Down|2016|-OqrcUvrbRY|Anderson Paak|S16022|Funk;Neo-Soul;West Coast Hip Hop;Hip Hop;R&B;Soul|",
 "Eminem|The Way I Am|2000|IaZReoUoDzU|Eminem|S1404|Hardcore Hip Hop;Hip Hop|The Way I Am (Eminem song)",
