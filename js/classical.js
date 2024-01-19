ab6557b9d2e1d92477de172cdabbc50f6116a7ef
Daniel Moore <hi@danielmoore.us>
2024-01-18 20:03:57 -0800

    Commit.

--- js/classical.js
+++ js/classical.js
@@ -624,7 +624,7 @@ const classical = [
 "Wilhelm Stenhammar|Serenade|1913|h0RSG4RCQJM|Wilhelm Stenhammar|",
 "Claude Debussy|Jeux|1913|rABBmzI4HQU|Claude Debussy|Jeux",
 "Edward Elgar|Falstaff|1913|8yM0c2J4x7g|Edward Elgar|Falstaff (Elgar)",
-"Ivor Gurney|Songs|1913-1922|_cA3gMjVwY0|Ivor Gurney|",
+"Ivor Gurney|Songs|1913-1922|OLAK5uy_k7esdHOvPbw1bItEKZ7mEh7kcs6KT74lA|Ivor Gurney|",
 "Sergei Rachmaninoff|The Bells|1913|UdUThLpuBJ4|Sergei Rachmaninoff|The Bells (symphony)",
 "Albert Roussel|Le festin de l’araignée|1913|RufoBzyv7Iw|Albert Roussel|The Spider's Feast",
 "Igor Stravinsky|The Rite of Spring|1913|EkwqPJZe8ms|Igor Stravinsky|",
