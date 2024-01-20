e17ac72db50065a13a5f16bfc1e61d7aff9b759a
Daniel Moore <hi@danielmoore.us>
2024-01-19 19:53:39 -0800

    Commit.

--- js/classical.js
+++ js/classical.js
@@ -352,7 +352,7 @@ const classical = [
 "Edvard Grieg|Songs|1861-1905|R_5AwR27wj4|Edvard Grieg|",
 "Gabriel Fauré|Mélodies|1861-1921|RRcfSNmm0c8|Gabriel Fauré|Trois mélodies, Op. 7 (Fauré)",
 "Franz Liszt|Faust Symphony|1861|3ZUQ7yZTFco|Franz Liszt|Faust Symphony",
-"Henri Vieuxtemps|Violin Concerto No. 5|1861|jyT-Jqwhvio|Henri Vieuxtemps|Violin Concerto No. 5 (Vieuxtemps)",
+"Henri Vieuxtemps|Violin Concerto No. 5|1861|oCFKcQsXeZ4|Henri Vieuxtemps|Violin Concerto No. 5 (Vieuxtemps)",
 "Giuseppe Verdi|La forza del destino|1862-1869|drbHCt0lwzo|Giuseppe Verdi|La forza del destino",
 "Henryk Wieniawski|Violin Concerto No. 2|1862|T7KNd6W5OXo|Henryk Wieniawski|",
 "Johannes Brahms|Piano Quintet in F minor|1862-1864|b-DqO_D1g1g|Johannes Brahms|Piano Quintet (Brahms)",
