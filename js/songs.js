f2e075bd0adbd260a1108dfc6c5d18f173c53e84
Daniel Moore <hi@danielmoore.us>
2024-01-22 23:10:11 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -10358,7 +10358,7 @@ const songs = [
 "The Age of Love|The Age of Love|1990|SOsGNzYTUwg|Pino D'Angiò|S15232|New Beat;Trance;Dance;EBM;Electronic;Electronic Dance Music;Industrial Music|",
 "Nancy Sinatra|Jackson|1967|rnkuRQ8tjIE?start=5|Nancy Sinatra|S7216|Baroque Pop;Country Pop;Country;Pop|",
 "The Beatmasters|Rok Da House|1987|eEgJv4Qn1JM|Beatmasters|S5384|Hip House;Dance;Electronic;Electronic Dance Music;Hip Hop;House|",
-"Orange Juice|What Presence?|1984|5zHoaLl9340|Orange Juice (band)|S2908|Indie Pop;Jangle Pop;Alternative Rock;Pop;Pop Rock;Rock|",
+"Orange Juice|What Presence?!|1984|5zHoaLl9340|Orange Juice (band)|S2908|Indie Pop;Jangle Pop;Alternative Rock;Pop;Pop Rock;Rock|",
 "The Fatback Band|King Tim III (Personality Jock)|1979|ojQqAnlrW_M|Fatback Band|S10788|Disco Rap;Funk;Hip Hop;R&B|King Tim III (Personality Jock)",
 "Lenny Kravitz|Let Love Rule|1989|TcU57tAKzng|Lenny Kravitz|S11411|Psychedelic Soul;R&B;Soul|",
 "Simian Mobile Disco|Audacity of Huge|2009|Ylu0ybj7DIg|Simian Mobile Disco|S8414|Electroclash;Electropop;Dance;Electronic;Electronic Dance Music;Pop|",
