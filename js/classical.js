723c1cf43bb01935942b92903f182b842185ffb1
Daniel Moore <hi@danielmoore.us>
2024-01-22 23:48:55 -0800

    Commit.

--- js/classical.js
+++ js/classical.js
@@ -201,7 +201,7 @@ const classical = [
 "Ludwig van Beethoven|Symphony No. 6, \"Pastoral\"|1808|nXgBtAD5daM|Ludwig van Beethoven|Symphony No. 6 (Beethoven)",
 "Ludwig van Beethoven|Piano Concerto No. 5 in E flat major|1809|3TiYGxOQDYw|Ludwig van Beethoven|Piano Concerto No. 5 (Beethoven)",
 "Fernando Sor|Guitar Works|1810-1830s|I2wTJAcRWg4|Fernando Sor|",
-"Ludwig van Beethoven|Piano Sonata in E flat major, Op. 81A, \"Les adieux\"|1810|Z26dfRI9rqg|Ludwig van Beethoven|Piano Sonata No. 26 (Beethoven)",
+"Ludwig van Beethoven|Les Adieux|1810|6c9B3K4ii-c|Ludwig van Beethoven|Piano Sonata No. 26 (Beethoven)",
 "Ludwig van Beethoven|Piano Trio in B flat major, Op. 97, \"Archduke\"|1811|ezRNLXBbB8Q|Ludwig van Beethoven|Piano Trio, Op. 97 (Beethoven)",
 "Carl Maria von Weber|Clarinet Concertos nos. 1 &amp; 2|1811|95gL5U4ILFc|Carl Maria von Weber|",
 "Ludwig van Beethoven|Symphony No. 7|1812|-4788Tmz9Zo|Ludwig van Beethoven|Symphony No. 7 (Beethoven)",
