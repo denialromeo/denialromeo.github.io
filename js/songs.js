461e6fb6765b281d10308da0c2b115ad2cd6470a
Daniel Moore <hi@danielmoore.us>
2024-01-23 16:36:16 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -2696,7 +2696,7 @@ const songs = [
 "Thundercat feat. Flying Lotus & Kamasi Washington|Them Changes|2015|GNCd_ERZvZM|Thundercat (musician)|S9856|Acid Jazz;Funk;Neo-Soul;Jazz;R&B;Soul|",
 "Drake feat. Wizkid and Kyla|One Dance|2016|qL7zrWcv6XY|Drake (musician)|S16077|Afrobeats;Contemporary R&B;Dance;Dance-Pop;Pop;R&B|One Dance",
 "Chuck Berry|You Never Can Tell|1964|ULVrZrj_veo|Chuck Berry|S5809|Rock & Roll;Rock|You Never Can Tell (song)",
-"Barbara Lynn|You'll Lose a Good Thing|1962|0x4AZHDT-4A|Barbara Lynn|S3578|Rhythm & Blues;Soul;R&B|You'll Lose a Good Thing",
+"Barbara Lynn|You'll Lose a Good Thing|1962|LoaLCc2jR_g|Barbara Lynn|S3578|Rhythm & Blues;Soul;R&B|You'll Lose a Good Thing",
 "Madonna|Open Your Heart|1986|snsTmi9N9Gs|Madonna (entertainer)|S1944|Dance-Pop;Dance;Pop|Open Your Heart (Madonna song)",
 "The Beach Boys|Sloop John B.|1966|nSAoEf1Ib58|The Beach Boys|S2280|Baroque Pop;Sunshine Pop;Pop|Sloop John B",
 "Santo and Johnny|Sleep Walk|1959|YBRCvVpknvg|Santo & Johnny|S3795|Surf Rock;Rock;Surf Music|Sleep Walk",
