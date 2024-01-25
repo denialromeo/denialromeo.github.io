ff1ceb1b0d179d92865b190a497060a4dc00b0f1
Daniel Moore <hi@danielmoore.us>
2024-01-25 14:32:10 -0800

    Commit.

--- js/classical.js
+++ js/classical.js
@@ -811,7 +811,7 @@ const classical = [
 "Sergei Prokofiev|Alexander Nevsky|1939|KGqVogrLEE4|Sergei Prokofiev|Alexander Nevsky (Prokofiev)",
 "William Walton|Violin Concerto|1939-1943|fLVTTn_jPKk|William Walton|Violin Concerto (Walton)",
 "Samuel Barber|Violin Concerto|1940|MdRD6gEa9CY|Samuel Barber|Violin Concerto (Barber)",
-"Benjamin Britten|Sinfonia da requiem|1940|IZAgYQSTTJA|Benjamin Britten|Sinfonia da Requiem",
+"Benjamin Britten|Sinfonia da Requiem|1940|IZAgYQSTTJA|Benjamin Britten|Sinfonia da Requiem",
 "Dmitri Shostakovich|Piano Quintet|1940|UEPiqK-jqTc|Dmitri Shostakovich|Piano Quintet (Shostakovich)",
 "Sergei Prokofiev|Piano Sonata No. 6|1940|p9GzBjB-YpA|Sergei Prokofiev|Piano Sonata No. 6 (Prokofiev)",
 "Aram Khachaturian|Violin Concerto|1940|rixdcNMGwWQ|Aram Khachaturian|Violin Concerto (Khachaturian)",
