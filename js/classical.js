770a40637e3f2df198cfc1c125653eb7bec17668
Daniel Moore <hi@danielmoore.us>
2024-01-26 12:51:42 -0800

    Commit.

--- js/classical.js
+++ js/classical.js
@@ -603,7 +603,7 @@ const classical = [
 "Franz Schreker|Der ferne Klang|1910|wgYr2weuhC0|Franz Schreker|Der ferne Klang",
 "Alexander Scriabin|Prometheus|1910|10ESN_t7txI?start=45|Alexander Scriabin|Prometheus: The Poem of Fire",
 "Richard Strauss|Der Rosenkavalier|1910|lxrmLsG7dPY?start=3|Richard Strauss|Der Rosenkavalier",
-"Ralph Vaughan Williams|Fantasia on a Theme of Thomas Tallis|1910-1919|ihx5LCF1yJY|Ralph Vaughan Williams|Fantasia on a Theme by Thomas Tallis",
+"Ralph Vaughan Williams|Fantasia on a Theme by Thomas Tallis|1910-1919|ihx5LCF1yJY|Ralph Vaughan Williams|Fantasia on a Theme by Thomas Tallis",
 "Igor Stravinsky|The Firebird|1910|RZkIAVGlfWk|Igor Stravinsky|The Firebird",
 "Edward Elgar|Symphony No. 2|1911|CY7vaUhxhDY|Edward Elgar|Symphony No. 2 (Elgar)",
 "Enrique Granados|Goyescas|1911|s0aVTPvYQ4Y|Enrique Granados|",
