6f8855dbec02dccc478741f6977fba8594435859
Daniel Moore <hi@danielmoore.us>
2024-01-28 01:40:18 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -4919,7 +4919,7 @@ const songs = [
 "Commodores|Brick House|1977|-5EmnQp3V48|Commodores|S2887|Funk;R&B|",
 "The Association|Windy|1967|RsY8l0Jg3lY|The Association|S5259|Sunshine Pop;Pop|Windy (The Association song)",
 "Matching Mole|O Caroline|1972|5aXw2J3AGZE|Matching Mole|S2862|Canterbury Scene;Progressive Rock;Rock|",
-"Ohio Players|Fire|1974|Y47G-Wa4qfs|Ohio Players|S7798|Funk;R&B|Fire (Ohio Players song)",
+"Ohio Players|Fire|1974|zqsYNc_M988|Ohio Players|S7798|Funk;R&B|Fire (Ohio Players song)",
 "Suicidal Tendencies|Institutionalized|1983|TE4Fvjhz2sE|Suicidal Tendencies|S3867|Crossover Thrash;Hardcore Punk;Metal;Punk Rock;Rock;Thrash Metal|",
 "Badly Drawn Boy|The Shining|2000|KNY-Pl1_ji0|Badly Drawn Boy|S4665|Indie Pop;Singer/Songwriter;Pop|",
 "Skream|Midnight Request Line|2005|p6WJYe6n-l8|Skream|S12221|Dubstep;Dance;Electronic;Electronic Dance Music|",
