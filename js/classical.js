6124145c1b38bda504ab1c465f4f2c77297f9003
Daniel Moore <hi@danielmoore.us>
2024-01-28 15:25:55 -0800

    Commit.

--- js/classical.js
+++ js/classical.js
@@ -406,7 +406,7 @@ const classical = [
 "Pyotr Ilyich Tchaikovsky|Variations on a Rococo Theme|1876|_591VkMY4iQ|Pyotr Ilyich Tchaikovsky|Variations on a Rococo Theme",
 "Pyotr Ilyich Tchaikovsky|The Seasons|1876|OLAK5uy_mEkjXexzva_ItaG6Cr4IxjJAGHvUvi-ZQ|Pyotr Ilyich Tchaikovsky|The Seasons (Tchaikovsky)",
 "Pyotr Ilyich Tchaikovsky|Swan Lake|1876|9rJoB7y6Ncs?start=3486|Pyotr Ilyich Tchaikovsky|Swan Lake",
-"Johannes Brahms|Symphony No. 2|1877|6nKd-ia7_Lc|Johannes Brahms|Symphony No. 2 (Brahms)",
+"Johannes Brahms|Symphony No. 2|1877|qbcfuMlNRWg?start=15|Johannes Brahms|Symphony No. 2 (Brahms)",
 "Camille Saint-Saens|Samson and Delilah|1877|X67Cguz5J0s?start=5|Camille Saint-Saëns|Samson and Delilah (opera)",
 "Johannes Brahms|Violin Concerto|1878|UFl9xuYP5T8?start=34|Johannes Brahms|Violin Concerto (Brahms)",
 "Pablo de Sarasate|Zigeunerweisen|1878|ufIlOXXMqs0|Pablo de Sarasate|",
