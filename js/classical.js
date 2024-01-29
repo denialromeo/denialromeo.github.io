b0ff23f89b8941983639b39b787a5da350d6378d
Daniel Moore <hi@danielmoore.us>
2024-01-28 17:53:33 -0800

    Commit.

--- js/classical.js
+++ js/classical.js
@@ -155,7 +155,7 @@ const classical = [
 "Wolfgang Amadeus Mozart|Eine kleine Nachtmusik|1787|156u4nV2uGE?start=4|Wolfgang Amadeus Mozart|Eine kleine Nachtmusik",
 "Wolfgang Amadeus Mozart|Don Giovanni|1787|aL2VdxseTvE|Wolfgang Amadeus Mozart|Don Giovanni",
 "Wolfgang Amadeus Mozart|Piano Concerto No. 26, \"Coronation\"|1788|RcBcMbOHzhI|Wolfgang Amadeus Mozart|Piano Concerto No. 26 (Mozart)",
-"Wolfgang Amadeus Mozart|Symphony No. 39|1788|H0x_dCrKd4w|Wolfgang Amadeus Mozart|Symphony No. 39 (Mozart)",
+"Wolfgang Amadeus Mozart|Symphony No. 39|1788|fAiF1PNlQ50?start=12|Wolfgang Amadeus Mozart|Symphony No. 39 (Mozart)",
 "Wolfgang Amadeus Mozart|Symphony No. 40|1788|QyQ-POuTNn8?start=4|Wolfgang Amadeus Mozart|Symphony No. 40 (Mozart)",
 "Wolfgang Amadeus Mozart|Symphony No. 41, \"Jupiter\"|1788|yZpFrydm6G4?start=14|Wolfgang Amadeus Mozart|Symphony No. 41 (Mozart)",
 "Wolfgang Amadeus Mozart|Prussian String Quartets|1789-1790|p_62BnrkIcs|Wolfgang Amadeus Mozart|Prussian Quartets (Mozart)",
