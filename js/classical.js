36968874d36301477fa0b5cd44ffa87c386dbcf8
Daniel Moore <hi@danielmoore.us>
2024-01-28 22:16:30 -0800

    Commit.

--- js/classical.js
+++ js/classical.js
@@ -630,7 +630,7 @@ const classical = [
 "Igor Stravinsky|The Rite of Spring|1913|EkwqPJZe8ms|Igor Stravinsky|",
 "Ralph Vaughan Williams|A London Symphony|1913-1933|0Uv4lJsu0Jw|Ralph Vaughan Williams|A London Symphony",
 "Erno Dohnányi|Variations on a Nursery Tune|1914|Bqx4oYOy85o|Ernst von Dohnányi|Variations on a Nursery Tune (Dohnányi)",
-"Manuel de Falla|Siete canciones populares Españolas|1914|YHMV1GFL3_g|Manuel de Falla|Siete canciones populares españolas",
+"Manuel de Falla|Siete canciones populares Españolas|1914|PLm0qx3c_LrRTlIB8021pET_m-2pbgatyP|Manuel de Falla|Siete canciones populares españolas",
 "Max Reger|Variations and Fugue on a Theme of Mozart|1914|2Uo0sm9mtR0|Max Reger|Variations and Fugue on a Theme by Mozart",
 "Ralph Vaughan Williams|The Lark Ascending|1914-1920|ZR2JlDnT2l8|Ralph Vaughan Williams|The Lark Ascending (Vaughan Williams)",
 "Zoltán Kodály|Duo for Violin and Cello|1914|ihgPqrukvLc|Zoltán Kodály|",
