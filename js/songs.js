4168d317cc247db719609d0877a5ef45a643a328
Daniel Moore <hi@danielmoore.us>
2024-01-30 19:19:06 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -8554,7 +8554,7 @@ const songs = [
 "Tori Amos|God|1994|cN3rzi7dZVQ|Tori Amos|S15948|Alternative Rock;Art Pop;Piano Rock;Singer/Songwriter;Pop;Pop Rock;Rock|God (Tori Amos song)",
 "Rammstein|Mein teil|2004|T87rQx3MnJs|Rammstein|S14919|Neue Deutsche Härte;Industrial Metal;Industrial Music;Metal;Rock|Mein Teil",
 "Benny Goodman & Charlie Christian|Stompin' at the Savoy|1936|x52x5hjpD5k|Charlie Christian|S13155|Big Band;Swing;Jazz|",
-"Ray Charles|America the Beautiful|1972|TRUjr8EVgBg|Ray Charles|S1744|Pop Soul;Pop;R&B;Soul|America the Beautiful",
+"Ray Charles|America the Beautiful|1972|46j1eX5Ehwk|Ray Charles|S1744|Pop Soul;Pop;R&B;Soul|America the Beautiful",
 "Aerosmith|Back in the Saddle|1976|A3vlj8mUiPA|Aerosmith|S3138|Hard Rock;Rock|Back in the Saddle",
 "The Hives|Tick Tick Boom|2007|1M02bAWDFkI|The Hives|S6717|Garage Rock Revival;Garage Rock;Rock|",
 "Eddy Duchin|Lovely to Look At|1935|0-9Xum70LmE|Eddy Duchin|S6201|Traditional Pop;Pop|",
