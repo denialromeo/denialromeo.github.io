02d8af6053b9767067211b3ada9a59f523da9595
Daniel Moore <hi@danielmoore.us>
2024-02-01 19:22:47 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -6432,7 +6432,7 @@ const songs = [
 "Pete Shelley|Homosapien|1981|2HwmO_GZfzI|Pete Shelley|S1873|New Wave;Synthpop;Electronic;Pop;Rock|Homosapien (song)",
 "Patti Smith|Frederick|1979|qFunRQfUk_Q|Patti Smith|S7868|Art Rock;Pop Rock;Pop;Rock|Frederick (song)",
 "Young Jeezy|Put On|2008|1aEImx74gLA|Jeezy|S4764|Southern Hip Hop;Trap;Gangsta Rap;Hardcore Hip Hop;Hip Hop|Put On",
-"Rosemary Clooney|Hey There|1954|T45e9EzFFHQ|Rosemary Clooney|S8246|Easy Listening;Traditional Pop;Pop|",
+"Rosemary Clooney|Hey There|1954|j_RXqwGndz8|Rosemary Clooney|S8246|Easy Listening;Traditional Pop;Pop|Hey There",
 "Archie Bleyer|Hernando's Hideaway|1954|XC05YwDUmVg|Archie Bleyer|S5228|Novelty;Traditional Pop;Descriptor;Pop|",
 "Fatboy Slim feat. Bootsy Collins|Weapon Of Choice|2000|wCDIYvFmgW8|Fatboy Slim|S13459|Big Beat;Funk;Breakbeat;Dance;Electronic;Electronic Dance Music;R&B|Weapon of Choice (song)",
 "Robert Charlebois & Louise Forestier|Lindbergh|1968|PSqhNWoUt-Q|Robert Charlebois|S8175|Psychedelic Rock;Rock|",
