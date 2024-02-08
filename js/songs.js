f1d79f597f21f4fce914b3b26c1835e227781cdd
Daniel Moore <hi@danielmoore.us>
2024-02-08 01:22:43 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -2094,7 +2094,7 @@ const songs = [
 "The Animals|Don't Let Me Be Misunderstood|1965|_2sz_YwwwQ4|The Animals|S1668|British Rhythm & Blues;Mod;R&B;Rhythm & Blues;Rock|Don't Let Me Be Misunderstood",
 "Yes|Roundabout|1971|kmZoQFYYx8U|Yes (band)|S3982|Progressive Rock;Rock|Roundabout (Yes song)",
 "Rod Stewart|Da Ya Think I'm Sexy?|1978|Hphwfq1wLJs|Rod Stewart|S1809|Disco;Pop Rock;Dance;Pop;Rock|Da Ya Think I'm Sexy%3F",
-"The Rolling Stones|Moonlight Mile|1971|Bq4Q69_mdZw|The Rolling Stones|S4544|Roots Rock;Rock|Moonlight Mile (song)",
+"The Rolling Stones|Moonlight Mile|1971|7PXOHpBneEU|The Rolling Stones|S4544|Roots Rock;Rock|Moonlight Mile (song)",
 "Eric Clapton|I Shot the Sheriff|1974|APWhx97QvxE|Eric Clapton|S2869|Pop Reggae;Pop Rock;Caribbean Music;Jamaican Music;Pop;Reggae;Regional Music;Rock|I Shot the Sheriff",
 "The Rapture|How Deep Is Your Love?|2011|AoedK3XcoBE|The Rapture (band)|S4988|Alternative Dance;Alternative Rock;Dance;Rock|How Deep Is Your Love",
 "FKA Twigs|Water Me|2013|kFtMl-uipA8|FKA Twigs|S9448|Ambient Pop;Art Pop;Pop|",
