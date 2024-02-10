e01aa51a14326cfc1667a3f16586d0faad0cdfcd
Daniel Moore <hi@danielmoore.us>
2024-02-10 12:50:31 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -2803,7 +2803,7 @@ const songs = [
 "Benjamin Biolay|La Superbe|2009|xtmVTfGJUzA|Benjamin Biolay|S10142|Chamber Pop;French Pop;Nouvelle chanson française;Baroque Pop;Chanson;Indie Pop;Pop;Regional Music|",
 "Smokey Robinson and the Miracles|Going to a Go-Go|1965|sWt4Hz1KGcQ|The Miracles|S977|Motown Sound;Pop;Pop Soul;R&B;Soul|",
 "Jackson Browne|Late for the Sky|1974|kqYiHkx7ils|Jackson Browne|S2583|Singer/Songwriter|Late for the Sky",
-"2Pac|Hail Mary|1996|_jGCL0CkXow|Tupac Shakur|S5070|Gangsta Rap;West Coast Hip Hop;Hardcore Hip Hop;Hip Hop|Hail Mary (2Pac song)",
+"2Pac|Hail Mary|1996|vw-O1JdXcXk|Tupac Shakur|S5070|Gangsta Rap;West Coast Hip Hop;Hardcore Hip Hop;Hip Hop|Hail Mary (2Pac song)",
 "Elvis Presley|Blue Moon|1956|MiY5auB3OWg|Elvis Presley|S1620|Pop|",
 "Christine and the Queens feat. Dâm-Funk|Girlfriend / Damn, dis-moi|2018|S0NMTuz9Duo|Christine and the Queens|S16540|Synth Funk;Synthpop;Electronic;Funk;Pop;R&B|",
 "Dire Straits|Romeo and Juliet|1980|mxfjSnMN88U|Dire Straits|S6089|Pop Rock;Pop;Rock|Romeo and Juliet (Dire Straits song)",
