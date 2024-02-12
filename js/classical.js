ee12c49c670805e87c07e526fa8b85c39ea790b9
Daniel Moore <hi@danielmoore.us>
2024-02-12 11:51:39 -0800

    Commit.

--- js/classical.js
+++ js/classical.js
@@ -324,7 +324,7 @@ const classical = [
 "Otto Nicolai|The Merry Wives of Windsor|1849|S0oHhdJ__ME|Otto Nicolai|The Merry Wives of Windsor (opera)",
 "Robert Schumann|Symphony No. 3, \"Rhenish\"|1850|3lRdCGIp-rg?start=3|Robert Schumann|Symphony No. 3 (Brahms)",
 "Robert Schumann|Cello Concerto in A minor|1850|NayPFVXYo9o|Robert Schumann|Cello Concerto (Schumann)",
-"Franz Liszt|Transcendental Études|1851|1O4h0AapdbQ|Franz Liszt|Transcendental Études",
+"Franz Liszt|Transcendental Études|1851|PLEQ0hp9avWWF2uZCkzh0M4RSmDN7FiTTZ|Franz Liszt|Transcendental Études",
 "Giuseppe Verdi|Rigoletto|1851|fYDI6MWkCW8|Giuseppe Verdi|Rigoletto",
 "Robert Schumann|Symphony No. 4|1851|Fq98d0JOiKQ?start=3|Robert Schumann|Symphony No. 2 (Schumann)",
 "Johannes Brahms|Lieder|1851-1896|QQ45pJwyO9o|Johannes Brahms|",
