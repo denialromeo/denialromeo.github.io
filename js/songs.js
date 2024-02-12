c157c82362e49b5ad13ffb95a33ad8533a387f23
Daniel Moore <hi@danielmoore.us>
2024-02-11 22:26:26 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -7940,7 +7940,7 @@ const songs = [
 "Michael Jackson|Dirty Diana|1987|yUi_S6YWjZw|Michael Jackson|S13566|Pop Rock;Pop;Rock|Dirty Diana",
 "Gnarls Barkley|Run|2008|2GA3a15xF0c|Gnarls Barkley|S6589|Neo-Soul;R&B;Soul|",
 "Steve Earle|Copperhead Road|1988|xvaEJzoaYZk|Steve Earle|S8568|Alt-Country;Country Rock;Singer/Songwriter;Country;Rock|",
-"E-40|Tell Me When to Go|2006|2GZbaXdK8Js|E-40|S4034|Hyphy;West Coast Hip Hop;Hip Hop|",
+"E-40|Tell Me When to Go|2006|2GZbaXdK8Js|E-40|S4034|Hyphy;West Coast Hip Hop;Hip Hop|Tell Me When to Go",
 "Maroon 5|Makes Me Wonder|2007|sAebYQgy4n4|Maroon 5|S7439|Funktronica;Pop Rock;Dance;Electronic;Electronic Dance Music;Pop;Rock|Makes Me Wonder",
 "Erroll Garner|I'll Remember April|1956|OfQvMpihfak|Erroll Garner|S10756|Bebop;Stride;American Folk Music;Folk;Jazz;Ragtime;Regional Music;Traditional Folk Music|I'll Remember April (song)",
 "Brass Construction|Movin'|1975|9eVyQikja8I|Brass Construction|S12854|Disco;Funk;Dance;R&B|Movin' (Brass Construction song)",
