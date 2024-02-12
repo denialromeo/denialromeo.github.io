48a6bc90a3e440c5d2e04d45ba915cb0fd518fbf
Daniel Moore <hi@danielmoore.us>
2024-02-11 20:47:49 -0800

    Commit.

--- js/classical.js
+++ js/classical.js
@@ -144,7 +144,7 @@ const classical = [
 "Wolfgang Amadeus Mozart|Piano Concerto No. 20|1785|devtF2EoZDs|Wolfgang Amadeus Mozart|Piano Concerto No. 20 (Mozart)",
 "Wolfgang Amadeus Mozart|Piano Concerto No. 21|1785|df-eLzao63I|Wolfgang Amadeus Mozart|Piano Concerto No. 21 (Mozart)",
 "Wolfgang Amadeus Mozart|Piano Concerto No. 22|1785|P9eEbEfEqoU?start=25|Wolfgang Amadeus Mozart|Piano Concerto No. 22 (Mozart)",
-"Wolfgang Amadeus Mozart|Horn Concerto No. 4|1786|VEJFuWgkGkY|Wolfgang Amadeus Mozart|Horn Concerto No. 4 (Mozart)",
+"Wolfgang Amadeus Mozart|Horn Concerto No. 4|1786|gtOkOYTM_5E?start=35|Wolfgang Amadeus Mozart|Horn Concerto No. 4 (Mozart)",
 "Wolfgang Amadeus Mozart|Piano Concerto No. 23|1786|TmrDa_ddon8|Wolfgang Amadeus Mozart|Piano Concerto No. 23 (Mozart)",
 "Wolfgang Amadeus Mozart|The Marriage of Figaro|1786|55ik-PzAXsQ|Wolfgang Amadeus Mozart|The Marriage of Figaro",
 "Wolfgang Amadeus Mozart|Piano Concerto No. 24|1786|POWVTXuB68I|Wolfgang Amadeus Mozart|Piano Concerto No. 24 (Mozart)",
