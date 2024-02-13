4bf9fc5de7f20c8f045c6a1d7e1e57e9ae5f5184
Daniel Moore <hi@danielmoore.us>
2024-02-13 14:28:37 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -5915,7 +5915,7 @@ const songs = [
 "Dixie Chicks|Wide Open Spaces|1998|dom7VlltBUc|Dixie Chicks|S4236|Country Pop;Country;Pop|Wide Open Spaces (song)",
 "Red Hot Chili Peppers|Suck My Kiss|1991|C6jElKMMOWM|Red Hot Chili Peppers|S8121|Funk Rock;Rap Rock;Alternative Rock;Rock|Aeroplane (Red Hot Chili Peppers song)",
 "Rufus & Chaka Khan|Sweet Thing|1975|bFKXGVtXq8s|Rufus (band)|S8266|Pop Soul;Pop;R&B;Soul|Sweet Thing (Rufus song)",
-"Johnny Mercer|Ac-cent-tchu-ate the Positive|1944|f3jdbFOidds|Johnny Mercer|S11246|Vocal Jazz;Jazz|Ac-Cent-Tchu-Ate the Positive",
+"Johnny Mercer|Ac-cent-tchu-ate the Positive|1944|On7AX37Aoms|Johnny Mercer|S11246|Vocal Jazz;Jazz|Ac-Cent-Tchu-Ate the Positive",
 "Percy Faith|The Theme from \"A Summer Place\"|1959|tQkKCnditQY|Percy Faith|S2803|Film Score;Light Music;Descriptor;Easy Listening;Film Soundtrack;Pop;Soundtracks|Theme from A Summer Place",
 "U2|Staring at the Sun|1997|6HxUKsZR4MA|U2|S3921|Alternative Rock;Pop Rock;Pop;Rock|Staring at the Sun (U2 song)",
 "The Marvelettes|The Hunter Gets Captured by the Game|1966|W-vNHRExA7E|The Marvelettes|S7451|Girl Group;Motown Sound;Pop;Pop Soul;R&B;Soul|The Hunter Gets Captured by the Game",
