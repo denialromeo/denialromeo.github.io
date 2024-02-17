b6ce39ac4dc0633c190678648a2470f082973025
Daniel Moore <hi@danielmoore.us>
2024-02-17 14:13:27 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -1310,7 +1310,7 @@ const songs = [
 "Smashing Pumpkins|Today|1993|xmUZ6nCFNoU|The Smashing Pumpkins|S142|Alternative Rock;Rock|Today (The Smashing Pumpkins song)",
 "New Order|Ceremony|1981|kkASIx9Xk6s|New Order (band)|S533|Post-Punk;Rock|Ceremony (New Order song)",
 "Thunderclap Newman|Something in the Air|1969|RTZoJ01FpD8|Thunderclap Newman|S881|Pop Rock;Psychedelic Pop;Pop;Rock|Something in the Air",
-"Van Morrison|Into the Mystic|1970|_6r2P4W9Yog|Van Morrison|S2559|Folk Rock;Rhythm & Blues;Singer/Songwriter;R&B;Rock|Into the Mystic",
+"Van Morrison|Into the Mystic|1970|m0gvodQgu-Y|Van Morrison|S2559|Folk Rock;Rhythm & Blues;Singer/Songwriter;R&B;Rock|Into the Mystic",
 "OutKast|Rosa Parks|1998|drsQLEU0N1Y|Outkast|S299|Conscious Hip Hop;Southern Hip Hop;Hip Hop|Rosa Parks (song)",
 "The Smiths|Panic|1986|wMykYSQaG_c|The Smiths|S369|Indie Pop;Jangle Pop;Alternative Rock;Pop;Pop Rock;Rock|Panic (The Smiths song)",
 "Interpol|Obstacle 1|2002|OC5zHACynR4|Interpol (band)|S3115|Post-Punk Revival;Alternative Rock;Indie Rock;Post-Punk;Rock|Obstacle 1",
