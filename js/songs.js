b7fcc582fdb2e2d6b201a64d07e6554ae6b2de9a
Daniel Moore <hi@danielmoore.us>
2024-02-18 11:08:34 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -6058,7 +6058,7 @@ const songs = [
 "David Axelrod|The Human Abstract|1969|5nurl0YewBA|David Axelrod (musician)|S5959|Baroque Pop;Psychedelic Pop;Pop|The Human Abstract|",
 "Elton John|Someone Saved My Life Tonight|1975|2TfuRv4Wa_g|Elton John|S6241|Piano Rock;Pop;Pop Rock;Rock|Someone Saved My Life Tonight",
 "Glenn Miller|Tuxedo Junction|1940|iBTYcqtaOjg|Glenn Miller|S6581|Big Band;Swing;Jazz|Tuxedo Junction",
-"Throbbing Gristle|United|1978|5XpqCxJZdGs|Throbbing Gristle|S8752|Industrial;Minimal Synth;Electronic;Experimental;Industrial Music;Minimal Wave|",
+"Throbbing Gristle|United|1978|5XpqCxJZdGs|Throbbing Gristle|S8752|Industrial;Minimal Synth;Electronic;Experimental;Industrial Music;Minimal Wave|United/Zyklon B Zombie",
 "Badfinger|Day After Day|1971|XonFZjuyc6E|Badfinger|S5302|Power Pop;Pop;Pop Rock;Rock|Day After Day (Badfinger song)",
 "Julieta Venegas|De mis pasos|1997|kcwfjNduQok|Julieta Venegas|S13529|Alternative Rock;Latin Alternative;Hispanic Music;Latin American Music;Regional Music;Rock|",
 "Steve Reich|Come Out|1968|aVXc9Veo_B8|Steve Reich|S15071|Spoken Word;Tape Music;Descriptor;Experimental|",
