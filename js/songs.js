2060961f1f081887040fc047d28f02a7d844cb43
Daniel Moore <hi@danielmoore.us>
2024-02-21 00:28:12 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -1941,7 +1941,7 @@ const songs = [
 "The Coasters|Young Blood|1957|e-Exe4YUJyI|The Coasters|S1625|Doo-Wop;Rhythm & Blues;Descriptor;R&B;Vocal Group|Young Blood (The Coasters song)",
 "Rihanna|Bitch Better Have My Money|2015|oQW2FFt3-A8|Rihanna|S9831|Trap;Gangsta Rap;Hardcore Hip Hop;Hip Hop|Bitch Better Have My Money",
 "Moby|Go|1990|N_Qwo8sT9U0|Moby|S202|Progressive House;Techno;Dance;Electronic;Electronic Dance Music;House|Go (Moby song)",
-"Spinners|Could It Be I'm Falling in Love|1972|EU4UTDrSwQc|The Spinners (American R&B group)|S1554|Philly Soul;R&B;Soul|Could It Be I'm Falling in Love",
+"Spinners|Could It Be I'm Falling in Love|1972|dln3ifkCfXs|The Spinners (American R&B group)|S1554|Philly Soul;R&B;Soul|Could It Be I'm Falling in Love",
 "Dua Lipa|New Rules|2017|k2qgadSvNyU|Dua Lipa|S16365|Dance-Pop;Electropop;Dance;Electronic;Pop|New Rules",
 "The Moonglows|Sincerely|1954|0xGqfmvNVuk|The Moonglows|S1293|Doo-Wop;Descriptor;R&B;Vocal Group|Sincerely (song)",
 "Deep Purple|Highway Star|1972|ubMQtQIbELs|Deep Purple|S3324|Hard Rock;Rock|Highway Star (song)",
