497b5308f58af6a3f6465404966180b8ce2cd644
Daniel Moore <hi@danielmoore.us>
2024-02-23 10:20:51 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -2216,7 +2216,7 @@ const songs = [
 "Duran Duran|Rio|1982|nTizYn3-QN0|Duran Duran|S3363|New Romantic;Synthpop;Electronic;New Wave;Pop;Rock|Rio (song)",
 "Anita Ward|Ring My Bell|1979|AvEcIeWhjyU|Anita Ward|S1824|Disco;Funk;Dance;R&B|Ring My Bell",
 "Donnie Trumpet & The Social Experiment|Sunday Candy|2015|i4ooH8frBWg|Nico Segal|S9741|Jazz Rap;Pop Rap;Hip Hop|",
-"McFadden and Whitehead|Ain't No Stoppin' Us Now|1979|DY0tsKCB4lc|McFadden & Whitehead|S591|Disco;Dance|Ain't No Stoppin' Us Now",
+"McFadden and Whitehead|Ain't No Stoppin' Us Now|1979|ZEWkZb11pss|McFadden & Whitehead|S591|Disco;Dance|Ain't No Stoppin' Us Now",
 "The Supremes|Love Child|1968|hIwdyIpmg-I|Diana Ross|S3874|Girl Group;Motown Sound;Pop;Pop Soul;R&B;Soul|Love Child (song)",
 "Dusty Springfield|The Look of Love|1967|mbaySb6HXqs|Dusty Springfield|S2281|Pop Soul;Pop;R&B;Soul|The Look of Love (1967 song)",
 "XTC|Senses Working Overtime|1982|ZRmLG_InAoI|XTC|S1885|Jangle Pop;New Wave;Alternative Rock;Pop;Pop Rock;Rock|Senses Working Overtime",
