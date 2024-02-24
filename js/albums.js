fec2f2cd5556df4ad274a56606e312f49b98f067
Daniel Moore <hi@danielmoore.us>
2024-02-23 21:26:38 -0800

    Commit.

--- js/albums.js
+++ js/albums.js
@@ -2248,7 +2248,7 @@ const albums = [
 "Bronski Beat|The Age of Consent|1984|OLAK5uy_ntHqQMQhKv-rKaXva2g-QMV3F_EWhwat8|A1489|The Age of Consent (album)|Bronski Beat|Hi-NRG;Synthpop;Dance;Disco;Electro-Disco;Electronic;Electronic Dance Music;Pop",
 "John Grant|Queen of Denmark|2010|OLAK5uy_mEqZAmSn6M8boTDlflcA5xJor2qXddouY|A5077|Queen of Denmark (album)|John Grant (musician)|Folk Pop;Singer/Songwriter;Contemporary Folk;Folk;Pop",
 "Faust|IV|1973|OLAK5uy_nwPO8uVvYVTZ2v_5WbaNdtVghTdGzZ02A|A2489|Faust IV|Faust (band)|Krautrock;Experimental Rock;Progressive Rock;Rock",
-"Scritti Politti|Songs to Remember|1982|OLAK5uy_kGLBEuGF6z6E1B0SW83fl9vHv845JrxCM|A570|Songs to Remember|Scritti Politti|New Wave;Sophisti-Pop;Pop;Rock",
+"Scritti Politti|Songs to Remember|1982|PLSCNIHqH-g9pMy8PX_1mzVSGT8MmnV2r_|A570|Songs to Remember|Scritti Politti|New Wave;Sophisti-Pop;Pop;Rock",
 "Iron & Wine|The Creek Drank the Cradle|2002|OLAK5uy_mf7hOIqXU9spYcquzmaLZ_81FH4LAD-Qw|A1754|The Creek Drank the Cradle|Iron & Wine|Contemporary Folk;Lo-Fi / Slacker Rock;Singer/Songwriter;Alternative Rock;Folk;Indie Rock;Rock",
 "Midnight Oil|10, 9, 8, 7, 6, 5, 4, 3, 2, 1|1982|OLAK5uy_m3uSJpsyDrF9PHgjJBoaVG8wBs-7_dSPA|A2806|10, 9, 8, 7, 6, 5, 4, 3, 2, 1|Midnight Oil|New Wave;Post-Punk;Rock",
 "Chico Buarque|Construção|1971|OLAK5uy_kSp2kBhC5BFcpH8B6hQe8M_5fXIWUcvq4|A3281|Construção|Chico Buarque|MPB;Singer/Songwriter;Brazilian Music;Regional Music",
