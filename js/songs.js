1a798063a5ac908074cc542189b699ac8fbf282b
Daniel Moore <hi@danielmoore.us>
2024-02-23 21:00:57 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -7025,7 +7025,7 @@ const songs = [
 "Gary Wright|Dream Weaver|1975|xZKuzwPOefs|Gary Wright|S6508|Progressive Pop;Soft Rock;Pop;Pop Rock;Rock|Dream Weaver",
 "Rihanna|Only Girl (In the World)|2010|pa14VNsdSYM|Rihanna|S8163|Dance-Pop;Electropop;Dance;Electronic;Pop|Only Girl (In the World)",
 "Merle Haggard|If We Make It Though December|1973|LsUm_5jHhJ4|Merle Haggard|S7522|Country|",
-"Melissa Etheridge|Come to My Window|1993|9fxMHHyOtgY|Melissa Etheridge|S3395|Heartland Rock;Pop Rock;Singer/Songwriter;Pop;Rock|Come to My Window",
+"Melissa Etheridge|Come to My Window|1993|Z4aj-t4eGrs|Melissa Etheridge|S3395|Heartland Rock;Pop Rock;Singer/Songwriter;Pop;Rock|Come to My Window",
 "Pink Floyd|Careful with That Axe, Eugene|1968|tMpGdG27K9o|Pink Floyd|S11904|Psychedelic Rock;Rock|",
 "The Smiths|Stop Me If You Think You've Heard This One Before|1987|SckD99B51IA|The Smiths|S8465|Indie Pop;Jangle Pop;Alternative Rock;Pop;Pop Rock;Rock|Stop Me If You Think You've Heard This One Before",
 "The Rolling Stones|Torn and Frayed|1972|khRFhrhcJrs|The Rolling Stones|S8228|Blues Rock;Rock|Torn and Frayed",
