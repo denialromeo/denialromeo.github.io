269fdd16e15e037f922d9aea20f2f5f73729c37d
Daniel Moore <hi@danielmoore.us>
2024-02-27 06:30:52 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -185,7 +185,7 @@ const songs = [
 "Kraftwerk|Autobahn|1974|vkOZNJYAZ7c|Kraftwerk|S693|Progressive Electronic;Electronic|Autobahn (song)",
 "Animal Collective|My Girls|2009|zol2MJf6XNE|Animal Collective|S4784|Neo-Psychedelia;Psychedelic Pop;Pop;Psychedelic Rock;Rock|My Girls (Animal Collective song)",
 "The Beatles|Eleanor Rigby|1966|HuS5NuXRb5Y|The Beatles|S1444|Baroque Pop;Pop|Eleanor Rigby",
-"T. Rex|Get It On [Bang a Gong (Get It On)]|1971|wZkTh_T75QY|T. Rex (band)|S815|Glam Rock;Rock|Get It On (T. Rex song)",
+"T. Rex|Bang a Gong (Get It On)|1971|gMzVeoJFw44|T. Rex (band)|S815|Glam Rock;Rock|Get It On (T. Rex song)",
 "The La's|There She Goes|1988|-CCfIJgVM6M|The La's|S221|Jangle Pop;Alternative Rock;Pop;Pop Rock;Rock|There She Goes (The La's song)",
 "The Beatles|Help!|1965|2Q_ZzBGPdqE|The Beatles|S998|Pop Rock;Pop;Rock|Help! (song)",
 "The Rapture|House of Jealous Lovers|2002|hKSTpmQjt7Y|The Rapture (band)|S1508|Dance-Punk;Post-Punk Revival;Alternative Rock;Indie Rock;Post-Punk;Rock|House of Jealous Lovers",
