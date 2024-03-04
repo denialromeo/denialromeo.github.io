df2872bc48a21b9f1859d7ef3d7547b95cc91d7b
Daniel Moore <hi@danielmoore.us>
2024-03-03 17:15:53 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -23,7 +23,7 @@ const songs = [
 "The Rolling Stones|Sympathy for the Devil|1968|Jwtyn-L-2gQ|The Rolling Stones|S1705|Blues Rock;Rock|Sympathy for the Devil",
 "The Beatles|Strawberry Fields Forever|1967|HtUH9z_Oey8|The Beatles|S931|Progressive Pop;Psychedelic Pop;Pop|Strawberry Fields Forever",
 "Little Richard|Tutti Frutti|1955|F13JNjpNW6c|Little Richard|S1296|Rock & Roll;Rock|Tutti Frutti (song)",
-"Led Zeppelin|Stairway to Heaven|1971|QkF3oxziUI4|Led Zeppelin|S2567|Folk Rock;Hard Rock;Progressive Rock;Rock|Stairway to Heaven",
+"Led Zeppelin|Stairway to Heaven|1971|Ly6ZhQVnVow|Led Zeppelin|S2567|Folk Rock;Hard Rock;Progressive Rock;Rock|Stairway to Heaven",
 "Grandmaster Flash and the Furious Five|The Message|1982|PobrSpMwKk4|Grandmaster Flash and the Furious Five|S481|Conscious Hip Hop;East Coast Hip Hop;Hip Hop|The Message (Grandmaster Flash and the Furious Five song)",
 "Ray Charles|What'd I Say|1959|EPLZL4s_jtI|Ray Charles|S1171|Rhythm & Blues;R&B|What'd I Say",
 "John Lennon|Imagine|1971|YkgkThdzX-8?start=40|John Lennon|S812|Pop;Singer/Songwriter|Imagine (John Lennon song)",
