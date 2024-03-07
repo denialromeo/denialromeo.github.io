f8c78247378fcbd4e6f90e4b1a5644885402e064
Daniel Moore <hi@danielmoore.us>
2024-03-06 21:12:45 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -1020,7 +1020,7 @@ const songs = [
 "New Order|Temptation|1982|xxDv_RTdLQo|New Order (band)|S486|Alternative Dance;New Wave;Post-Punk;Alternative Rock;Dance;Rock|",
 "Robyn|With Every Heartbeat|2007|-ojHWQrm4UM|Robyn|S4289|Electropop;Synthpop;Electronic;Pop|With Every Heartbeat",
 "The Chords|Sh-Boom|1954|9jt_q1jM89Y|The Chords (American band)|S1335|Doo-Wop;Descriptor;R&B;Vocal Group|Sh-Boom",
-"Paul McCartney|Live and Let Die|1973|e7aGAIWe3uE|Paul McCartney and Wings|S1752|Pop Rock;Progressive Pop;Pop;Rock|",
+"Paul McCartney|Live and Let Die|1973|NR0UmZcf89E|Paul McCartney and Wings|S1752|Pop Rock;Progressive Pop;Pop;Rock|",
 "Ray Charles|I Can't Stop Loving You|1962|r_7iRVtxui8|Ray Charles|S1096|Country Soul;Traditional Pop;Pop;R&B;Soul|I Can't Stop Loving You",
 "Bob Dylan|All Along the Watchtower|1967|bT7Hj-ea0VE|Bob Dylan|S1692|Contemporary Folk;Folk Rock;Singer/Songwriter;Folk;Rock|All Along the Watchtower",
 "Madonna|Music|2000|Sdz2oW0NMFk|Madonna (entertainer)|S1381|Dance-Pop;Electropop;Dance;Electronic;Pop|Music (Madonna song)",
