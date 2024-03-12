01bb01a382a51cedbcd6484d2a39107bffd27f21
Daniel Moore <hi@danielmoore.us>
2024-03-12 11:39:21 -0700

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -8822,7 +8822,7 @@ const songs = [
 "Ariel Pink|Black Ballerina|2014|ECpGJmyT-ic|Ariel Pink|S10025|Hypnagogic Pop;Psychedelic Pop;Neo-Psychedelia;Pop;Psychedelic Rock;Rock|Pom Pom (album)",
 "Soulwax|E Talking|2004|SdbaDBp1S_I|Soulwax|S8502|Alternative Dance;Dance-Punk;Electroclash;Post-Punk Revival;Alternative Rock;Dance;Electronic;Electronic Dance Music;Indie Rock;Post-Punk;Rock|Soulwax|E Talking",
 "Musique|In the Bush|1978|w7xfOeGa15Q|Musique (disco band)|S12885|Disco;Dance|",
-"Nicolas Jaar|Space Is Only Noise If You Can See|2011|V-_c0o8LAaM|Nicolas Jaar|S7739|Ambient House;Microhouse;Dance;Electronic;Electronic Dance Music;House|",
+"Nicolas Jaar|Space Is Only Noise If You Can See|2011|ag5aTwk19qU|Nicolas Jaar|S7739|Ambient House;Microhouse;Dance;Electronic;Electronic Dance Music;House|",
 "Bruce Springsteen|Cover Me|1984|E6sJ9bZ8B14|Bruce Springsteen|S5650|Heartland Rock;Pop Rock;Pop;Rock|Cover Me (Bruce Springsteen song)",
 "Pépé Bradock|Deep Burnt|1999|QTvFzBS6e68|https://fr.wikipedia.org/wiki/P%C3%A9p%C3%A9_Bradock|S11876|Deep House;French House;Dance;Electronic;Electronic Dance Music;House|Music for the Uninvited",
 "King Princess|1950|2018|LNxWTS25Tbk|King Princess|S16607|Alternative R&B;Pop Soul;Contemporary R&B;Pop;R&B;Soul|",
