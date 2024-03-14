0f94fb8131043a1098cd31dee5955b4435578a3c
Daniel Moore <hi@danielmoore.us>
2024-03-13 18:25:31 -0700

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -9811,7 +9811,7 @@ const songs = [
 "Jim Croce|Operator (That's Not the Way It Feels)|1972|48Kiv8uhL7o|Jim Croce|S13832|Singer/Songwriter|Operator (That's Not the Way It Feels)",
 "Jay-Z feat. Beanie Sigel|Ignorant Shit|2007|5gSW3lec9gU|Jay-Z|S15572|East Coast Hip Hop;Pop Rap;Hip Hop|",
 "Monyaka|Go deh yaka|1983|zhnSNRP1KDs|Monyaka|S7602|Reggae;Caribbean Music;Jamaican Music;Regional Music|",
-"Ryan Adams|La Cienega Just Smiled|2001|OWvbjENrgbY|Ryan Adams|S4249|Folk Pop;Singer/Songwriter;Contemporary Folk;Folk;Pop|",
+"Ryan Adams|La Cienega Just Smiled|2001|IM3-PmQBvpY|Ryan Adams|S4249|Folk Pop;Singer/Songwriter;Contemporary Folk;Folk;Pop|",
 "The 45 King|The 900 Number|1988|kVtg_qiGx34|The 45 King|S15223|Boom Bap;East Coast Hip Hop;Hip Hop|",
 "Billy Jones|Yes We Have No Bananas|1923|PDd8shcLvHI|Billy Jones (singer, born 1889)|S5459|Novelty;Descriptor|Yes! We Have No Bananas",
 "Javiera Mena|Luz de piedra de luna|2010|atjmVa8k-vc|Javiera Mena|S13513|Electro-Disco;Synthpop;Dance;Disco;Electronic;Electronic Dance Music;Pop|",
