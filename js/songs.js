eca1f7814208a2d8bc7163293a528a0d43620530
Daniel Moore <hi@danielmoore.us>
2024-03-22 11:37:31 -0700

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -9103,7 +9103,7 @@ const songs = [
 "The B-52's|52 Girls|1979|rJwQuTqqPvo|The B-52's|S12766|New Wave;Rock|",
 "Doris Day|Love Somebody|1948|mkiI_aFiPMg|Doris Day|S6145|Traditional Pop;Pop|Love Somebody (1947 song)",
 "Low|Disarray|2018|dXaLOVeWOdU|Low (band)|S16626|Ambient Pop;Glitch Pop;Electronic;Pop|",
-"Frankie Carle|Rumors Are Flying|1946|CWset0bXaD8|Frankie Carle|S6454|Traditional Pop;Pop|Rumors Are Flying",
+"Frankie Carle|Rumors Are Flying|1946|_lSVLbSpmcA|Frankie Carle|S6454|Traditional Pop;Pop|Rumors Are Flying",
 "Hoagy Carmichael|Ole Buttermilk Sky|1946|9wdLXuykRss|Hoagy Carmichael|S6718|Traditional Pop;Pop|",
 "The Beach Boys|Wild Honey|1967|eeA3Z8As_dc|The Beach Boys|S5345|Pop Soul;Psychedelic Pop;Pop;R&B;Soul|Wild Honey (The Beach Boys song)",
 "Jean Ferrat|Nuit et Brouillard|1963|CwGaG5IMiyE|Jean Ferrat|S13514|Chanson;Regional Music|",
