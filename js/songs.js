33af771488d65a203a32ec46438d332fbcf68a26
Daniel Moore <hi@danielmoore.us>
2024-04-13 21:47:20 -0700

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -6192,7 +6192,7 @@ const songs = [
 "Felix|Don't You Want Me|1992|bj8xF2bnxp0|Felix (musician)|S2939|Euro House;Techno;Dance;Electronic;Electronic Dance Music;House|Don't You Want Me (Felix song)",
 "Barbra Streisand|Woman in Love|1980|CzQPJy8Q6qE|Barbra Streisand|S5323|Adult Contemporary;Pop|People (Barbra Streisand song)",
 "Don Henley|The End of the Innocence|1989|3QIqLQngJ_E|Don Henley|S9207|Pop Rock;Pop;Rock|",
-"Sugar|Changes|1992|h3Lo91Z0h3Q|Sugar (American band)|S8626|Alternative Rock;Power Pop;Pop;Pop Rock;Rock|Sugar Mama",
+"Sugar|Changes|1992|FAagGjSSwZE|Sugar (American band)|S8626|Alternative Rock;Power Pop;Pop;Pop Rock;Rock|Sugar Mama",
 "Sun Ra|Space Is the Place|1973|dokLwszdUgY|Sun Ra|S12833|Avant-Garde Jazz;Jazz|",
 "Metric|Help I'm Alive|2009|wTJm3_n6kuw|Metric (band)|S7526|Indie Rock;Alternative Rock;Rock|Help I'm Alive",
 "Mumford & Sons|I Will Wait|2012|mw0jCyH5cZA|Mumford & Sons|S7638|Americana;Indie Folk;Contemporary Folk;Country;Folk|I Will Wait",
