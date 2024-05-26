ac11401ba2d5a3b017fd2b11ca369d77df026eda
Daniel Moore <hi@danielmoore.us>
2024-05-25 18:00:17 -0700

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -1741,7 +1741,7 @@ const songs = [
 "Steve Harley & Cockney Rebel|Make Me Smile (Come Up and See Me)|1975|dAoaVU3-ve0|Steve Harley & Cockney Rebel|S704|Glam Rock;Pop Rock;Pop;Rock|Make Me Smile (Come Up and See Me)",
 "Paul Robeson|Ol' Man River|1936|eh9WayN7R-s|Paul Robeson|S4871|Show Tunes;Spirituals;Standards;American Folk Music;Descriptor;Folk;Musical Theatre and Entertainment;Pop;Regional Music;Soundtracks;Traditional Folk Music;Traditional Pop|Ol' Man River",
 "Blind Lemon Jefferson|Matchbox Blues|1927|JXC1jjRCXtg|Blind Lemon Jefferson|S2702|Acoustic Texas Blues;Acoustic Blues;American Folk Music;Blues;Country Blues;Folk;Regional Music;Traditional Folk Music|",
-"Fischerspooner|Emerge|2001|M4uSTdJo4zk|Fischerspooner|S1469|Electroclash;Synthpop;Dance;Electronic;Electronic Dance Music;Pop|Emerge (song)",
+"Fischerspooner|Emerge|2001|HHbAtxSTXo0|Fischerspooner|S1469|Electroclash;Synthpop;Dance;Electronic;Electronic Dance Music;Pop|Emerge (song)",
 "Suede|The Drowners|1992|BGFY7qaRGPc|Suede (band)|S153|Britpop;Alternative Rock;Pop;Pop Rock;Rock|",
 "Gene Autry|Back in the Saddle Again|1939|rSqcxFGFVas|Gene Autry|S2646|Cowboy;American Folk Music;Country;Folk;Regional Music;Traditional Country;Traditional Folk Music|Back in the Saddle Again",
 "Stealers Wheel|Stuck in the Middle with You|1972|OMAIsqvTh7g|Stealers Wheel|S1754|Folk Rock;Pop Rock;Pop;Rock|",
