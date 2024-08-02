65562e0cc3ae4d827138887fe5a3cdd52169b59a
Daniel Moore <hi@danielmoore.us>
2024-08-01 23:33:08 -0700

    Commit.

--- bin/export.bat
+++ bin/export.bat
@@ -4,6 +4,4 @@ dir
 @python export_saved.py
 @popd
 @cd "C:\Users\hi\Desktop\code\export-saved-reddit"
-@copy /y "C:\Users\hi\Desktop\code\export-saved-reddit\export-saved.html" "C:\Users\hi\Desktop\code\danielmoore.us\bin"
-@copy /y "C:\Users\hi\Desktop\code\views" "C:\Users\hi\Desktop\code\danielmoore.us\bin"
 @exit
