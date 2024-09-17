44843b1001917773d88bb2965cef315296560767
Daniel Moore <hi@danielmoore.us>
2024-09-16 19:27:45 -0700

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -7975,7 +7975,7 @@ const songs = [
 "The Police|Voices Inside My Head|1980|io3CNOeDfO4|The Police|S13596|Pop Reggae;Caribbean Music;Jamaican Music;Reggae;Regional Music|Zenyatta Mondatta",
 "Gorillaz|19/2000|2001|WXR-bCF5dbM|Gorillaz|S6615|Alternative Rock;Trip Hop;Downtempo;Electronic;Rock|19-2000",
 "David Soul|Don't Give Up on Us|1976|YY8APrYU2Gs|David Soul|S5984|Adult Contemporary;Pop|Don't Give Up on Us (song)",
-"Lennie Tristano|Turkish Mambo|1956|G6dQHdBfkmg|Lennie Tristano|S13225|Bebop;Cool Jazz;Jazz|",
+"Lennie Tristano|Turkish Mambo|1956|LjXb5_lHK3s|Lennie Tristano|S13225|Bebop;Cool Jazz;Jazz|",
 "Sonic Youth|Bull in the Heather|1994|8JGBNkLM9_8|Sonic Youth|S8492|Alternative Rock;Noise Rock;Rock|Bull in the Heather",
 "Robbie Williams|She's the One|1998|sVaRHJn_dgM|Robbie Williams|S8172|Adult Contemporary;Pop Rock;Pop;Rock|She's the One (World Party song)",
 "Jackie DeShannon|When You Walk in the Room|1963|IZf6YdPVwNY|Jackie DeShannon|S6823|Blue Eyed Soul;Pop|When You Walk in the Room",
