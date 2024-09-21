829fb0449f9033b055811a35e365e727c4b76e26
Daniel Moore <hi@danielmoore.us>
2024-09-20 21:24:38 -0700

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -1868,7 +1868,7 @@ const songs = [
 "Spoon|Inside Out|2014|IpT5SBg1Mmk|Spoon (band)|S9599|Indie Pop;Indie Rock;Neo-Psychedelia;Alternative Rock;Pop;Psychedelic Rock;Rock|They Want My Soul",
 "The Dixie Cups|Chapel of Love|1964|rTq7w8P6_2I|The Dixie Cups|S1032|Brill Building;Girl Group;Pop|Chapel of Love",
 "Jimi Hendrix|Foxey Lady|1967|_PVjcIO4MT4|Jimi Hendrix|S1493|Acid Rock;Blues Rock;Psychedelic Rock;Rock|Angel (Jimi Hendrix song)",
-"The Standells|Dirty Water|1965|5apEctKwiD8|The Standells|S1431|Garage Rock;Proto-Punk;Rock|Dirty Water",
+"The Standells|Dirty Water|1965|Pvhbm3AKgMI|The Standells|S1431|Garage Rock;Proto-Punk;Rock|Dirty Water",
 "The Specials|Too Much Too Young|1979|rdu8VOWk3pg|The Specials|S2600|2 Tone;Ska|",
 "The Isley Brothers|Twist and Shout|1962|cTaqn8_gMR0|The Isley Brothers|S1093|Rhythm & Blues;Rock & Roll;R&B;Rock|Shout (Isley Brothers song)",
 "Nat King Cole|Nature Boy|1947|Iq0XJCJ1Srw|Nat King Cole|S2663|Standards;Vocal Jazz;Jazz;Pop;Traditional Pop|Nature Boy",
