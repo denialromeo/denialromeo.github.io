94df4632147efad4e9c66ddf1af6304ade09def9
Daniel Moore <hi@danielmoore.us>
2024-09-24 15:33:15 -0700

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -9098,7 +9098,7 @@ const songs = [
 "The Monkees|(I'm Not Your) Steppin' Stone|1966|ehWbMmFm6G0|The Monkees|S11674|Pop Rock;Pop;Rock|(I'm Not Your) Steppin' Stone",
 "The Replacements|Here Comes a Regular|1985|_n1E8EgnkIo|The Replacements (band)|S4575|Alternative Rock;Power Pop;Pop;Pop Rock;Rock|",
 "Juan Carlos Baglietto|Mirta, de regreso|1982|uyGpk6LXZuE|Juan Carlos Baglietto|S11264|Folk Rock;Singer/Songwriter;Rock|",
-"Johnnie Ray|Just Walkin' in the Rain|1956|8uCsvWgmjwg|Johnnie Ray|S11237|Traditional Pop;Pop|Just Walkin' in the Rain",
+"Johnnie Ray|Just Walkin' in the Rain|1956|PmdLJ7JZslQ|Johnnie Ray|S11237|Traditional Pop;Pop|Just Walkin' in the Rain",
 "Bernd Begemann|Hitler - Menschlich gesehen|1993|zU9OanZvhvY|Bernd Begemann|S14127|Hamburger Schule;Lo-Fi / Slacker Rock;Singer/Songwriter;Alternative Rock;Indie Rock;Rock|",
 "The B-52's|52 Girls|1979|rJwQuTqqPvo|The B-52's|S12766|New Wave;Rock|",
 "Doris Day|Love Somebody|1948|oyca3r1IlbQ|Doris Day|S6145|Traditional Pop;Pop|Love Somebody (1947 song)",
