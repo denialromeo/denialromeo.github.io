1c75f3af21baba2a041d1b5ffb785d0449ef9f26
Daniel Moore <hi@danielmoore.us>
2024-09-25 19:54:12 -0700

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -8557,7 +8557,7 @@ const songs = [
 "Ray Charles|America the Beautiful|1972|46j1eX5Ehwk|Ray Charles|S1744|Pop Soul;Pop;R&B;Soul|America the Beautiful",
 "Aerosmith|Back in the Saddle|1976|A3vlj8mUiPA|Aerosmith|S3138|Hard Rock;Rock|Back in the Saddle",
 "The Hives|Tick Tick Boom|2007|1M02bAWDFkI|The Hives|S6717|Garage Rock Revival;Garage Rock;Rock|Tick Tick Boom (song)",
-"Eddy Duchin|Lovely to Look At|1935|0-9Xum70LmE|Eddy Duchin|S6201|Traditional Pop;Pop|",
+"Eddy Duchin|Lovely to Look At|1935|1zbiMc6plpM|Eddy Duchin|S6201|Traditional Pop;Pop|",
 "Injury Reserve feat. Aminé|Jailbreak the Tesla|2019|PWQL_XORalY|Injury Reserve|S16795|Hardcore Hip Hop;Industrial Hip Hop;Experimental Hip Hop;Hip Hop;Industrial Music|",
 "Roxanne Shanté|Roxanne's Revenge|1984|0eckRNcHCKA|Roxanne Shante|S12724|East Coast Hip Hop;Hip Hop|",
 "Billy Thorpe & The Aztecs|Most People I Know (Think That I'm Crazy)|1972|XtJq56cp_dk|Billy Thorpe|S10180|Pop Rock;Pop;Rock|",
