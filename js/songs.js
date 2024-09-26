210da83b900d7561ebfade8057b6903b5d417d8a
Daniel Moore <hi@danielmoore.us>
2024-09-25 23:39:15 -0700

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -8708,7 +8708,7 @@ const songs = [
 "The Cadillacs|Gloria|1954|KAUQb8TVo2U|The Cadillacs|S9174|Doo-Wop;Descriptor;R&B;Vocal Group|Gloria (Leon René song)",
 "Javiera Mena|Espada|2013|GqNTdNCU2y8|Javiera Mena|S13511|Electropop;Nu-Disco;Synthpop;Dance;Disco;Electronic;Electronic Dance Music;House;Pop|",
 "Serge Lama|Je suis malade|1973|HDbpZpm9j9c?start=3|Serge Lama|S12182|Chanson;Regional Music|Je suis malade (song)",
-"Benny Goodman|Body and Soul|1935|Ko2c5yJp8G8|Benny Goodman|S10145|Standards;Swing;Jazz;Pop;Traditional Pop|",
+"Benny Goodman|Body and Soul|1935|rVZESAHznUg|Benny Goodman|S10145|Standards;Swing;Jazz;Pop;Traditional Pop|",
 "ceo|Come with Me|2010|4T74frXBpgk|Ceo (musician)|S5746|Balearic Beat;Indie Pop;Indietronica;Dance;Electronic;Electronic Dance Music;House;Pop|You Belong with Me",
 "Playgroup|Make It Happen|2000|peT9By2w5G8|Playgroup (band)|S4605|Electropop;Electronic;Pop|",
 "Kings of Leon|Radioactive|2010|wPBbMbKSZrQ|Kings of Leon|S7118|Alternative Rock;Southern Rock;Rock|Radioactive (Kings of Leon song)",
