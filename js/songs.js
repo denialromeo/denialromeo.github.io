15bd03677f8f27fc234a2531f5c33520b20053f2
Daniel Moore <hi@danielmoore.us>
2024-10-25 15:16:10 -0700

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -9873,7 +9873,7 @@ const songs = [
 "Rosemary Clooney|Tenderly|1952|CoeYnQHYfkc|Rosemary Clooney|S9342|Traditional Pop;Pop|Tenderly",
 "The Main Ingredient|Everybody Plays the Fool|1972|KPeHHpXOOds|The Main Ingredient (band)|S11523|Soul;R&B|Everybody Plays the Fool",
 "Roy Acuff and the Smoky Mountain Boys|Wreck on the Highway|1942|L114-E4v4EE|Roy Acuff|S15843|Traditional Country;American Folk Music;Country;Folk;Regional Music;Traditional Folk Music|",
-"Nelly Furtado|Say It Right|2006|PXgORnUiUsw|Nelly Furtado|S9304|Contemporary R&B;Dance-Pop;Dance;Pop;R&B|Say It Right",
+"Nelly Furtado|Say It Right|2006|6JnGBs88sL0|Nelly Furtado|S9304|Contemporary R&B;Dance-Pop;Dance;Pop;R&B|Say It Right",
 "The Embassy|It Never Entered My Mind|2002|3OBn1DpR5h8|The Embassy (band)|S10724|Indie Pop;Synthpop;Electronic;Pop|",
 "The Byrds|You Ain't Goin' Nowhere|1968|Q21BF38W3Gs|The Byrds|S4112|Country Rock;Rock|You Ain't Goin' Nowhere",
 "Rudy Vallée|Stein Song (University of Maine Fighting Song)|1930|EXrSmLhSxck|Rudy Vallée|S8264|Big Band;Traditional Pop;Jazz;Pop|",
