8f67897d45b43d14cb7b2cbd8714d824cbc44ce1
Daniel Moore <hi@danielmoore.us>
2024-10-29 19:04:50 -0700

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -2432,7 +2432,7 @@ const songs = [
 "Devo|(I Can't Get No) Satisfaction|1978|QmKQ2Z1odSc|Devo|S1813|Art Punk;New Wave;Post-Punk;Punk Rock;Rock|(I Can't Get No) Satisfaction",
 "The Kingston Trio|Tom Dooley|1958|VhXuO4Gz3Wo|The Kingston Trio|S1230|Contemporary Folk;Folk|Tom Dooley (song)",
 "Solomon Burke|Cry to Me|1962|mEu8DrO9PbY|Solomon Burke|S1103|Rhythm & Blues;Soul;R&B|Cry to Me",
-"Miles Davis|'Round Midnight|1957|qiNyBAMi1wY|Miles Davis|S11648|Cool Jazz;Jazz|",
+"Miles Davis|'Round Midnight|1957|Ys9wiGNDCvA|Miles Davis|S11648|Cool Jazz;Jazz|",
 "Dave Clark Five|Glad All Over|1963|NHtNFaa2ne0?start=5|The Dave Clark Five|S1650|Merseybeat;Beat Music;Pop;Pop Rock;Rock|Glad All Over",
 "Hot Hot Heat|Bandages|2002|i_K36y-iLUk|Hot Hot Heat|S2264|Post-Punk Revival;Power Pop;Alternative Rock;Indie Rock;Pop;Pop Rock;Post-Punk;Rock|",
 "Röyksopp|Do It Again|2014|btBSxtKzF6Q|Röyksopp|S9563|Electro House;Electropop;Dance;Electronic;Electronic Dance Music;House;Pop|Do It Again (Röyksopp and Robyn song)",
