b97b916dd51e4ec03edcac936c8d113be3d68b73
Daniel Moore <hi@danielmoore.us>
2024-11-06 22:15:34 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -4275,7 +4275,7 @@ const songs = [
 "The Rolling Stones|Bitch|1971|3N0A2b7nbdM|The Rolling Stones|S8220|Hard Rock;Rock|Bitch (Rolling Stones song)",
 "Elvis Costello|Accidents Will Happen|1979|aU_zMvaX05Q|Elvis Costello|S6244|New Wave;Pop Rock;Pop;Rock|Accidents Will Happen",
 "Joe Tex|Show Me|1967|oC3RpfRNoYc|Joe Tex|S4408|Southern Soul;R&B;Soul|Show Me (Joe Tex song)",
-"Tindersticks|Kathleen|1994|7CYcxN9955M|Tindersticks|S2087|Chamber Pop;Baroque Pop;Indie Pop;Pop|",
+"Tindersticks|Kathleen|1994|FIkLCk1HAP8|Tindersticks|S2087|Chamber Pop;Baroque Pop;Indie Pop;Pop|",
 "Jacques Brel|Orly|1977|Lq5iD-xV9wI|Jacques Brel|S6834|Chanson à texte;Chanson;Regional Music;Singer/Songwriter|",
 "Téléphone|Un autre monde|1984|xqnZPHo6qx4|Téléphone|S8711|Pop Rock;Pop;Rock|",
 "Kanye West|Famous|2016|odoAECsBgA0|Kanye West|S16113|Pop Rap;Hip Hop|Famous (Kanye West song)",
