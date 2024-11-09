6310f35058f54ba714a721135aa8b3cb6c3b1d28
Daniel Moore <hi@danielmoore.us>
2024-11-09 15:43:11 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -8287,7 +8287,7 @@ const songs = [
 "The Real Roxanne|Bang Zoom (Let's Go Go)|1986|rNtNUBxDjVY|The Real Roxanne|S372|East Coast Hip Hop;Electro;Dance;Electronic;Electronic Dance Music;Hip Hop|",
 "Paul Weller|The Changingman|1995|0v9WhRpQw8E|Paul Weller|S7902|Rock|The Changingman",
 "Commodores|Still|1979|5_GpDX2m1uk|Commodores|S5858|Adult Contemporary;Pop Soul;Pop;R&B;Soul|Still (Commodores song)",
-"Bob Dylan|I'll Be Your Baby Tonight|1967|c0FnoNC7KMQ|Bob Dylan|S3370|Country;Country Rock;Singer/Songwriter;Rock|I'll Be Your Baby Tonight",
+"Bob Dylan|I'll Be Your Baby Tonight|1967|1N8SRE97NrM|Bob Dylan|S3370|Country;Country Rock;Singer/Songwriter;Rock|I'll Be Your Baby Tonight",
 "Amos Milburn and His Aladdin Chickenshackers|One Scotch, One Bourbon, One Beer|1953|RZrP18m0lFo|Amos Milburn|S13381|Rhythm & Blues;R&B|",
 "Wheatus|Teenage Dirtbag|2000|FC3y9llDXuM|Wheatus|S8984|Alternative Rock;Power Pop;Pop;Pop Rock;Rock|Teenage Dirtbag",
 "Paul McCartney and Wings|Junior's Farm|1974|5Jpc9oZynyw|Paul McCartney and Wings|S11865|Pop Rock;Pop;Rock|Junior's Farm",
