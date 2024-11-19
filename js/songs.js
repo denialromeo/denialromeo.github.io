81e3dee9d641eb8fa79dda5600682ca9d9bb5946
Daniel Moore <hi@danielmoore.us>
2024-11-19 06:19:26 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -2992,7 +2992,7 @@ const songs = [
 "Throbbing Gristle|Hot on the Heels of Love|1979|trEo8O_ZftI|Throbbing Gristle|S8753|Electro-Disco;Minimal Synth;Synthpop;Dance;Disco;Electronic;Electronic Dance Music;Minimal Wave;Pop|",
 "Adam and the Ants|Kings of the Wild Frontier|1980|EGDVg0NZQQI|Adam and the Ants|S2896|New Wave;Post-Punk;Rock|Kings of the Wild Frontier (Adam and the Ants song)",
 "Café Tacvba|Eres|2003|98Akpf1ph2o|Café Tacuba|S5694|Alternative Rock;Pop Rock;Pop;Rock|Eres (Café Tacuba song)",
-"Scissor Sisters|Comfortably Numb|2003|lIQ4O0VNsxk|Scissor Sisters|S2402|Dance-Pop;Electropop;Nu-Disco;Dance;Disco;Electronic;Electronic Dance Music;House;Pop|Comfortably Numb",
+"Scissor Sisters|Comfortably Numb|2003|SqIQzNNK9d0|Scissor Sisters|S2402|Dance-Pop;Electropop;Nu-Disco;Dance;Disco;Electronic;Electronic Dance Music;House;Pop|Comfortably Numb",
 "Black Kids|I'm Not Gonna Teach Your Boyfriend How to Dance with You|2007|rOV6I4fYnvQ|Black Kids|S4703|Indie Pop;Pop|I'm Not Gonna Teach Your Boyfriend How to Dance with You",
 "Against Me!|Transgender Dysphoria Blues|2014|9K7PWLzdzP4|Against Me!|S9593|Punk Rock;Rock|",
 "Howlin' Wolf|The Red Rooster|1961|rmi4McTCaLI|Howlin' Wolf|S3483|Chicago Blues;Blues;Electric Blues|Little Red Rooster",
