01f5ab6091618576f0fd897e7187e6a95844c8fa
Daniel Moore <hi@danielmoore.us>
2024-11-22 12:54:10 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -8428,7 +8428,7 @@ const songs = [
 "Miles Davis|Summertime|1959|5FAYe2N4yRI|Miles Davis|S14757|Big Band;Jazz|",
 "Jacques Brel|Mon enfance|1967|tqtqUABH4ig|Jacques Brel|S11116|Chanson à texte;Chanson;Regional Music;Singer/Songwriter|",
 "Christopher Cross|Arthur's Theme (The Best That You Can Do)|1981|WOBHXxiZyZM|Christopher Cross|S5803|Film Soundtrack;Soft Rock;Yacht Rock;Descriptor;Pop;Pop Rock;Rock;Soundtracks|Arthur's Theme (Best That You Can Do)",
-"alt-J|In Cold Blood|2017|xaT_awMGiL8|Alt-J|S16326|Art Pop;Indietronica;Electronic;Pop|In Cold Blood (alt-J song)",
+"alt-J|In Cold Blood|2017|vlMePO0qywA|Alt-J|S16326|Art Pop;Indietronica;Electronic;Pop|In Cold Blood (alt-J song)",
 "The DeFranco Family|Heartbeat, It's a Lovebeat|1973|I3Iw2qYtN8A|The DeFranco Family|S13736|Bubblegum;Teen Pop;Pop|",
 "Lonnie Holley|I Woke Up in a Fucked-Up America|2018|Ss3cz9FgGnA|Lonnie Holley|S16630|Post-Industrial;Psychedelic Soul;Industrial Music;R&B;Soul|",
 "Dr. Feelgood|Roxette|1974|Yp2DvPKh118|Dr. Feelgood (band)|S6151|Pub Rock;Rock|",
