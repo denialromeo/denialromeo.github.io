1b9cb9ebf5b6b2c14567ad5290d948dd59019bae
Daniel Moore <hi@danielmoore.us>
2024-12-01 09:28:00 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -1139,7 +1139,7 @@ const songs = [
 "Glenn Miller|Moonlight Serenade|1939|bMdb2TB7-YI|Glenn Miller|S2718|Big Band;Swing;Jazz|",
 "The Jam|The Eton Rifles|1979|C7kUDkK70qQ|The Jam|S580|Mod Revival;New Wave;Power Pop;Mod;Pop;Pop Rock;Rock|The Eton Rifles",
 "Flamin' Groovies|Shake Some Action|1976|7cxM51kPdeQ|Flamin' Groovies|S680|Power Pop;Pop;Pop Rock;Rock|",
-"Lily Allen|Smile|2006|pY5bwU_n4BA|Lily Allen|S3057|Pop;Pop Reggae;Caribbean Music;Jamaican Music;Reggae;Regional Music|",
+"Lily Allen|Smile|2006|0WxDrVUrSvI|Lily Allen|S3057|Pop;Pop Reggae;Caribbean Music;Jamaican Music;Reggae;Regional Music|",
 "The Trashmen|Surfin' Bird|1963|Qgmh0BjLKVM|The Trashmen|S1651|Garage Rock;Surf Rock;Rock;Surf Music|Surfin' Bird",
 "Go-Go's|Our Lips Are Sealed|1981|r3kQlzOi27M|The Go-Go's|S537|New Wave;Pop Rock;Pop;Rock|Our Lips Are Sealed",
 "Smashing Pumpkins|Tonight, Tonight|1995|NOG3eus4ZSo|The Smashing Pumpkins|S3818|Alternative Rock;Symphonic Rock;Rock|Tonight, Tonight (The Smashing Pumpkins song)",
