9e61f5020a27a292639caad4d9e7c817db76efaa
Daniel Moore <hi@danielmoore.us>
2024-12-07 07:07:11 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -5608,7 +5608,7 @@ const songs = [
 "Jesse Belvin|Goodnight My Love (Pleasant Dreams)|1956|24jpHhaQy4g|Jesse Belvin|S3208|Rhythm & Blues;R&B|",
 "Kim Wilde|Kids in America|1981|8BYKpQ_6GbM|Kim Wilde|S3960|New Wave;Pop Rock;Pop;Rock|Kids in America",
 "Prince|Dirty Mind|1980|c3GPPnVz1fw|Prince (musician)|S11945|Synth Funk;Funk;R&B|Dirty Mind (Prince song)",
-"Neko Case|Hold On, Hold On|2006|a0vk5SGmw3w|Neko Case|S7693|Alt-Country;Singer/Songwriter;Country|",
+"Neko Case|Hold On, Hold On|2006|T5K0I_ZnoHA|Neko Case|S7693|Alt-Country;Singer/Songwriter;Country|",
 "Mahalia Jackson|His Eye Is on the Sparrow|1951|eM_JRAPSwVM|Mahalia Jackson|S4594|Gospel;American Folk Music;Folk;Regional Music;Traditional Folk Music|",
 "England Dan & John Ford Coley|I'd Really Love to See You Tonight|1976|UuUVgKT4roQ|England Dan & John Ford Coley|S6285|Soft Rock;Pop;Pop Rock;Rock|I'd Really Love to See You Tonight",
 "Ella Fitzgerald|Mack the Knife|1960|wYaEVSjg5BE|Ella Fitzgerald|S3405|Standards;Vocal Jazz;Jazz;Pop;Traditional Pop|Mack the Knife",
