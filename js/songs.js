a957b1b68f2f4aa04c0269b9664160649d0a6e2d
Daniel Moore <hi@danielmoore.us>
2024-12-09 21:56:35 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -3698,7 +3698,7 @@ const songs = [
 "Four Tops|Standing in the Shadows of Love|1966|6zhpRu2GuAo|Four Tops|S1433|Motown Sound;Pop;Pop Soul;R&B;Soul|",
 "Bob Hope|Thanks for the Memory|1938|pIZZoaFzXaA|Shirley Ross|S2697|Traditional Pop;Pop|Thanks for the Memory",
 "Liquid Liquid|Cavern|1983|FT4fZnNiWnY|Liquid Liquid|S4566|Dance-Punk;Post-Punk;Rock|",
-"The Jesus and Mary Chain|Upside Down|1984|UnO41-rKUsc|The Jesus and Mary Chain|S3515|Noise Pop;Noise Rock;Post-Punk;Alternative Rock;Rock|Upside Down (The Jesus and Mary Chain song)",
+"The Jesus and Mary Chain|Upside Down|1984|4s1jcPnpG58|The Jesus and Mary Chain|S3515|Noise Pop;Noise Rock;Post-Punk;Alternative Rock;Rock|Upside Down (The Jesus and Mary Chain song)",
 "Arcade Fire|We Used to Wait|2010|kJ7osdJ4H_8|Arcade Fire|S4886|Art Rock;Indie Rock;Alternative Rock;Rock|",
 "Stereolab|Cybele's Reverie|1996|0JgxATTtROs|Stereolab|S4516|Art Pop;Chamber Pop;Baroque Pop;Indie Pop;Pop|",
 "Rihanna|Needed Me|2016|4pdBg88UOAk|Rihanna|S16176|Alternative R&B;Pop Rap;Contemporary R&B;Hip Hop;R&B|Needed Me",
