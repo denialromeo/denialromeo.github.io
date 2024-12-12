789c933c167925adbfcb316eeb060ee76304bd39
Daniel Moore <hi@danielmoore.us>
2024-12-12 10:13:30 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -7902,7 +7902,7 @@ const songs = [
 "Sonic Youth|Silver Rocket|1988|viF12Mu3-5w|Sonic Youth|S12257|Alternative Rock;Noise Rock;Rock|",
 "X|Johnny Hit and Run Paulene|1980|UoTWFQE1xp4|X (American band)|S9073|Punk Rock;Rock|Los Angeles (X album)",
 "The Marshall Tucker Band|Can't You See|1973|-48Za7VZR_c|The Marshall Tucker Band|S7443|Southern Rock;Rock|Can't You See (The Marshall Tucker Band song)",
-"Gram Parsons|A Song for You|1973|98Ow_Jav_eo|Gram Parsons|S14453|Country Rock;Singer/Songwriter;Rock|Lazy Days (Gram Parsons song)",
+"Gram Parsons|A Song for You|1973|unohYEFELoo|Gram Parsons|S14453|Country Rock;Singer/Songwriter;Rock|Lazy Days (Gram Parsons song)",
 "Rihanna|What's My Name?|2010|s_LPsRHRXlk|Rihanna|S8164|Contemporary R&B;Dance-Pop;Dance;Pop;R&B|What's My Name%3F (Rihanna song)",
 "Traffic|Feeling Alright?|1968|xJcIjG9N1Qs|Traffic (band)|S8831|Baroque Pop;Psychedelic Pop;Psychedelic Rock;Pop;Rock|Feeling Alright",
 "James Brown|Think|1960|fpmpnVbfAXU|James Brown|S6844|Rhythm & Blues;R&B|Think (The \"5\" Royales song)",
