839f08c2262a64021c5c9fd7ad3aee233b4d35e5
Daniel Moore <hi@danielmoore.us>
2024-12-12 14:34:17 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -1086,7 +1086,7 @@ const songs = [
 "The Temptations|Ain't Too Proud to Beg|1966|_ObVQPBD0Uw|The Temptations|S1436|Motown Sound;Pop;Pop Soul;R&B;Soul|",
 "George Gershwin|Rhapsody in Blue|1924|cH2PH0auTUU|George Gershwin|S2684|Big Band;Modern Classical;Classical Music;Jazz|Rhapsody in Blue",
 "Tennessee Ernie Ford|Sixteen Tons|1955|S1980WfKC0o|Tennessee Ernie Ford|S1291|Country Boogie;Rhythm & Blues;Vocal Jazz;American Folk Music;Country;Folk;Jazz;R&B;Regional Music;Traditional Country;Traditional Folk Music|",
-"The Drifters|On Broadway|1963|yPYRtjxYEH8|The Drifters|S1061|Brill Building;Pop Soul;Pop;R&B;Soul|On Broadway (song)",
+"The Drifters|On Broadway|1963|5XtYvR8ryj8|The Drifters|S1061|Brill Building;Pop Soul;Pop;R&B;Soul|On Broadway (song)",
 "The Surfaris|Wipe Out|1963|dBURLdhmmZ8|The Surfaris|S1066|Rock & Roll;Surf Rock;Rock;Surf Music|Wipe Out (instrumental)",
 "Elastica|Connection|1994|ilKcXIFi-Rc|Elastica|S93|Britpop;Alternative Rock;Pop;Pop Rock;Rock|Connection (Elastica song)",
 "The Trammps|Disco Inferno|1976|L6imql3iDQQ|The Trammps|S637|Disco;Dance|Disco Inferno",
