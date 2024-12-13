805cfad192a831beecacb46c15093c228e6886ee
Daniel Moore <hi@danielmoore.us>
2024-12-12 19:30:01 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -9172,7 +9172,7 @@ const songs = [
 "The Cardigans|You're the Storm|2003|KMTLfChPzMc|The Cardigans|S5713|Indie Pop;Pop Rock;Pop;Rock|",
 "fIREHOSE|Flyin' the Flannel|1991|Mx7r4NsXT4k|Firehose (band)|S3404|Alternative Rock;Rock|",
 "Lightning Bolt|Dracula Mountain|2003|JGyANR4fiDw|Lightning Bolt (band)|S11435|Noise Rock;Rock|",
-"David Bowie|Letter to Hermione|1969|vIBO7XS7pe8|David Bowie|S10523|Contemporary Folk;Singer/Songwriter;Folk|",
+"David Bowie|Letter to Hermione|1969|Xw1mmqMD9j0|David Bowie|S10523|Contemporary Folk;Singer/Songwriter;Folk|",
 "Cibo Matto|Birthday Cake|1995|PmGyACG5K0E?start=57|Cibo Matto|S5816|Alternative Dance;Art Pop;Alternative Rock;Dance;Pop;Rock|",
 "The Flaming Lips|Silver Trembling Hands|2009|WTCpumkw4Kk|The Flaming Lips|S10821|Experimental Rock;Neo-Psychedelia;Psychedelic Rock;Rock|",
 "Michel Delpech|Quand j'étais chanteur|1975|9Z27xAaE_dU|Michel Delpech|S13572|Chanson;Regional Music|",
