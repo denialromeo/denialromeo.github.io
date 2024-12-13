324fd448e73148380e63411ef3857e9853194178
Daniel Moore <hi@danielmoore.us>
2024-12-12 21:44:40 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -8346,7 +8346,7 @@ const songs = [
 "Metallica|The Four Horsemen|1983|WOc7HyRZisc|Metallica|S15703|Thrash Metal;Metal;Rock|Mechanix",
 "Rickie Lee Jones|The Horses|1989|S-dMuyk5Pfc|Rickie Lee Jones|S4210|Singer/Songwriter|",
 "Dinosaur Jr.|Almost Ready|2007|DNcoroZEQUU?start=3|Dinosaur Jr.|S6079|Indie Rock;Alternative Rock;Rock|",
-"Ennio Morricone|The Ecstasy of Gold|1966|ZNGe7iK1O-4|Ennio Morricone|S12699|Film Score;Spaghetti Western;Cinematic Classical;Classical Music;Film Soundtrack;Soundtracks|",
+"Ennio Morricone|The Ecstasy of Gold|1966|ZNGe7iK1O-4|Ennio Morricone|S12699|Film Score;Spaghetti Western;Cinematic Classical;Classical Music;Film Soundtrack;Soundtracks|The Ecstasy of Gold",
 "Michel Delpech|Chez Laurette|1965|KJNDUJflo_c|Michel Delpech|S11627|Chanson;Regional Music|",
 "King Curtis|Memphis Soul Stew|1967|yRuLc2d5x5g|King Curtis|S11322|Funk;Rhythm & Blues;Soul;R&B|Memphis Soul Stew",
 "Perfume Genius|Mr. Peterson|2010|1-Aup2-Zs74|Perfume Genius|S7932|Indie Pop;Singer/Songwriter;Pop|",
