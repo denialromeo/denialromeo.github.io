7dccf7ab97f360d15dcf835af2e8fc20e9804112
Daniel Moore <hi@danielmoore.us>
2024-12-18 15:44:54 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -6589,7 +6589,7 @@ const songs = [
 "Guided by Voices|Teenage FBI|1999|INN4DF2BgLs|Guided by Voices|S2184|Indie Rock;Power Pop;Alternative Rock;Pop;Pop Rock;Rock|",
 "The Lightning Seeds|Three Lions|1996|RJqimlFcJsM|The Lightning Seeds|S5300|Britpop;Alternative Rock;Pop;Pop Rock;Rock|",
 "Pacific Gas & Electric|Are You Ready?|1970|5MR1LBEJ-ok|Pacific Gas & Electric (band)|S7839|Blues Rock;Rock|Are You Ready? (Pacific Gas & Electric song)",
-"R. Kelly|I'm a Flirt (Remix)|2007|x_JOV5rqKnI|R. Kelly|S4297|Contemporary R&B;R&B|I'm a Flirt",
+"R. Kelly|I'm a Flirt (Remix)|2007|czBZeP8UR-g|R. Kelly|S4297|Contemporary R&B;R&B|I'm a Flirt",
 "Andrew Bird|Fake Palindromes|2005|6NIiWU9d1KI|Andrew Bird|S4692|Chamber Pop;Indie Folk;Singer/Songwriter;Baroque Pop;Contemporary Folk;Folk;Indie Pop;Pop|",
 "Charly García|Nos siguen pegando abajo \"pecado mortal\"|1983|X-j3vl7SsrY|Charly García|S13428|Art Rock;New Wave;Rock|",
 "Blur|She's So High|1990|zxFOaoJuP5E|Blur (band)|S3222|Baggy / Madchester;Neo-Psychedelia;Alternative Rock;Psychedelic Rock;Rock|She's So High (Blur song)",
