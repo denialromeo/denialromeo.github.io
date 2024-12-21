51ae00165ab4d7cd36925f2e30ea6e347192514c
Daniel Moore <hi@danielmoore.us>
2024-12-21 14:46:43 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -2177,7 +2177,7 @@ const songs = [
 "The Dells|Oh What a Nite|1956|Z1ozQT8yQXA|The Dells|S1287|Doo-Wop;R&B;Vocal Group|Oh, What a Night (The Dells song)",
 "The Monotones|Book of Love|1957|OS1LFGGGazc|The Monotones|S1225|Doo-Wop;R&B;Vocal Group|The Book of Love (The Monotones song)",
 "Charles Aznavour|La Boheme|1965|hWLc0J52b2I|Charles Aznavour|S5757|Chanson;Regional Music|La Bohème (Charles Aznavour song)",
-"Joni Mitchell|River|1971|KxuIL1W4wGE|Joni Mitchell|S3632|Christmas Music;Piano Rock;Singer/Songwriter;Holiday Music;Pop;Pop Rock;Rock|River (Joni Mitchell song)",
+"Joni Mitchell|River|1971|OLHxxBTl71I|Joni Mitchell|S3632|Christmas Music;Piano Rock;Singer/Songwriter;Holiday Music;Pop;Pop Rock;Rock|River (Joni Mitchell song)",
 "Mississippi John Hurt|Stack O' Lee Blues|1928|BUJIKErqhr4|Mississippi John Hurt|S2699|Piedmont Blues;Acoustic Blues;American Folk Music;Blues;Country Blues;Folk;Regional Music;Traditional Folk Music|",
 "B.B. King|3 O'Clock Blues|1951|d9ozjCQkqZs|B.B. King|S5281|Electric Blues;Blues|3 O'Clock Blues",
 "Marshall Jefferson|Move Your Body (House Music Anthem)|1986|yFdbueklpwY|Marshall Jefferson|S1942|Chicago House;Dance;Electronic;Electronic Dance Music;House|Move Your Body (Marshall Jefferson song)",
