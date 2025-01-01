b4bdb86f4cc505ed46bcec5b0340fafbaceedccd
Daniel Moore <hi@danielmoore.us>
2024-12-31 22:44:39 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -7400,7 +7400,7 @@ const songs = [
 "Mott the Hoople|The Golden Age of Rock 'n' Roll|1974|jToqxJfUZTc|Mott the Hoople|S7619|Glam Rock;Rock|",
 "Brad Mehldau|Exit Music (for a Film)|1998|Mm-pBjBp4OA|Brad Mehldau|S13160|Jazz|",
 "Fake Blood|Mars|2008|8Fgm21dwf2o|Theo Keating|S13771|Fidget House;Dance;Electro House;Electronic;Electronic Dance Music;House|Mars (song)",
-"The Wrens (II)|She Sends Kisses|2003|i8xaUuZaIsI|The Wrens|S4685|Indie Rock;Alternative Rock;Rock|",
+"The Wrens (II)|She Sends Kisses|2003|2jK5qC6jdzY|The Wrens|S4685|Indie Rock;Alternative Rock;Rock|",
 "The Sabres of Paradise|Wilmot|1994|VK4lDJRkHYw|The Sabres of Paradise|S115|Dub;Electronic;Caribbean Music;Jamaican Music;Reggae;Regional Music|",
 "The Beatles|Free As a Bird|1995|ODIvONHPqpk|The Beatles|S5383|Pop Rock;Pop;Rock|Free as a Bird",
 "Argent|Hold Your Head Up|1971|k5fYPz8dKbg|Argent|S5232|Hard Rock;Progressive Rock;Rock|Hold Your Head Up",
