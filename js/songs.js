fdff40365a27c0ec8de95fb76cd57513ed1a90d5
Daniel Moore <hi@danielmoore.us>
2024-12-31 18:43:35 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -3406,7 +3406,7 @@ const songs = [
 "Edgar Winter Group|Free Ride|1973|rTEtdgdfXAo|Edgar Winter|S1755|Hard Rock;Pop Rock;Pop;Rock|Free Ride (song)",
 "Herman's Hermits|No Milk Today|1966|cV9fGx7ygAc|Herman's Hermits|S6711|Baroque Pop;Pop Rock;Pop;Rock|No Milk Today",
 "Bill Withers|Use Me|1972|LuzlbR5V_hc|Bill Withers|S5446|Rhythm & Blues;Smooth Soul;R&B;Soul|Use Me (Bill Withers song)",
-"Nina Simone|I Loves You, Porgy|1958|XheNXQd-Ju0|Nina Simone|S2801|Vocal Jazz;Jazz|",
+"Nina Simone|I Loves You, Porgy|1958|XheNXQd-Ju0|Nina Simone|S2801|Vocal Jazz;Jazz|I Loves You, Porgy",
 "Deep Purple|Black Night|1970|IRlZdnbcEDI|Deep Purple|S6015|Hard Rock;Rock|Black Night",
 "Billy Idol|White Wedding|1982|AAZQaYKZMTI|Billy Idol|S3492|New Wave;Pop Rock;Pop;Rock|White Wedding (song)",
 "Cat Stevens|Father and Son|1970|P6zaCV4niKk|Cat Stevens|S5736|Folk Pop;Singer/Songwriter;Contemporary Folk;Folk;Pop|Father and Son (song)",
