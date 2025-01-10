41edf47a2b68d0158f7599f45770ab6ec79e8898
Daniel Moore <hi@danielmoore.us>
2025-01-10 15:18:55 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -2525,7 +2525,7 @@ const songs = [
 "Edith Piaf|Non, je ne regrette rien|1960|JKPvx38D4GM|Édith Piaf|S6207|Chanson;Regional Music|Non, je ne regrette rien",
 "Gang of Four|At Home He's a Tourist|1979|ElhAysq3O6c|Gang of Four (band)|S1821|Dance-Punk;Post-Punk;Rock|",
 "Kelela|LMK|2017|ePi5BLJogyA|Kelela|S16410|Alternative R&B;Contemporary R&B;R&B|LMK",
-"Sleater-Kinney|I Wanna Be Your Joey Ramone|1996|JRsZ5ZwMNGE|Sleater-Kinney|S3814|Indie Rock;Riot Grrrl;Alternative Rock;Punk Rock;Rock|",
+"Sleater-Kinney|I Wanna Be Your Joey Ramone|1996|PDnV42VmleU|Sleater-Kinney|S3814|Indie Rock;Riot Grrrl;Alternative Rock;Punk Rock;Rock|",
 "Visage|Fade to Grey|1980|DDetfpEmGNM|Visage (band)|S9048|New Romantic;Synthpop;Electronic;New Wave;Pop;Rock|Fade to Grey (Visage song)",
 "Louis Jordan|Let the Good Times Roll|1946|NiVnLYK48X4|Louis Jordan|S4360|Jump-Blues;Rhythm & Blues;Blues;R&B|Let the Good Times Roll (Louis Jordan song)",
 "Duffy|Mercy|2008|y7ZEVA5dy-Y|Duffy (singer)|S4714|Blue Eyed Soul;Pop Soul;Pop;R&B;Soul|Mercy (Duffy song)",
