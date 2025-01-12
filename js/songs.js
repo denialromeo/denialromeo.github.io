f654186a5e1cae938a9269ddf19b0b6d32a84753
Daniel Moore <hi@danielmoore.us>
2025-01-11 18:02:22 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -671,7 +671,7 @@ const songs = [
 "De La Soul|Me Myself and I|1989|jdtKT5q-CW8|De La Soul|S247|Conscious Hip Hop;Disco Rap;East Coast Hip Hop;Hip Hop|Me Myself and I (De La Soul song)",
 "Sex Pistols|Pretty Vacant|1977|CwpyZ48MawQ|Sex Pistols|S634|Punk Rock;Rock|Pretty Vacant",
 "Magazine|Shot by Both Sides|1978|ybUqM8jf3mU|Magazine (band)|S607|Post-Punk;Rock|Shot by Both Sides",
-"John Lee Hooker|Boom Boom|1962|MTln9hsgORQ|John Lee Hooker|S1098|Chicago Blues;Rhythm & Blues;Blues;Electric Blues;R&B|Boom Boom (John Lee Hooker song)",
+"John Lee Hooker|Boom Boom|1962|yLOXCzobsT4|John Lee Hooker|S1098|Chicago Blues;Rhythm & Blues;Blues;Electric Blues;R&B|Boom Boom (John Lee Hooker song)",
 "Bob Dylan|Knockin' on Heaven's Door|1973|rm9coqlk8fY|Bob Dylan|S761|Folk Rock;Singer/Songwriter;Rock|Knockin' on Heaven's Door",
 "Cliff Richard|Move It|1958|k5mIq3yF6Nc|Cliff Richard|S1219|Rock & Roll;Rock|Move It",
 "The Marvelettes|Please Mr. Postman|1961|UDZ6Zp_zGzg|The Marvelettes|S1121|Girl Group;Pop Soul;Pop;R&B;Soul|Please Mr. Postman",
