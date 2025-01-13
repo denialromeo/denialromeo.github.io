dd6cd79e694e2b4ef7d4dde6a0173aa2e51fe112
Daniel Moore <hi@danielmoore.us>
2025-01-12 21:36:48 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -2194,7 +2194,7 @@ const songs = [
 "Leonard Cohen|The Future|1992|AY84Ar9bHZY|Leonard Cohen|S2044|Blue Eyed Soul;Singer/Songwriter;Sophisti-Pop;Pop|The Future (Leonard Cohen album)",
 "Japandroids|Young Hearts Spark Fire|2009|pRSSKtQHhxA|Japandroids|S4835|Lo-Fi / Slacker Rock;Noise Rock;Alternative Rock;Indie Rock;Rock|",
 "Slim Harpo|I'm a King Bee|1957|XWLvm11MAaM|Slim Harpo|S1260|Swamp Blues;Blues;Electric Blues|I'm a King Bee",
-"R.E.M.|Nightswimming|1992|KVT0S0j7F9Y|R.E.M.|S8060|Chamber Pop;Piano Rock;Baroque Pop;Indie Pop;Pop;Pop Rock;Rock|Nightswimming",
+"R.E.M.|Nightswimming|1992|ahJ6Kh8klM4|R.E.M.|S8060|Chamber Pop;Piano Rock;Baroque Pop;Indie Pop;Pop;Pop Rock;Rock|Nightswimming",
 "AC/DC|It's a Long Way to the Top (If You Wanna Rock and Roll)|1975|KRHpmvfxOg8|AC/DC|S4180|Hard Rock;Rock|It's a Long Way to the Top (If You Wanna Rock 'n' Roll)",
 "Shuggie Otis|Strawberry Letter 23|1971|11hGcmuccwk|Shuggie Otis|S3684|Psychedelic Soul;R&B;Soul|Strawberry Letter 23",
 "Arctic Monkeys|When the Sun Goes Down|2006|EqkBRVukQmE|Arctic Monkeys|S4031|Garage Rock Revival;Indie Rock;Alternative Rock;Garage Rock;Rock|When the Sun Goes Down (Arctic Monkeys song)",
