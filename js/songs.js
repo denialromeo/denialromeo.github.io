c7a6cec53adfdb818f6725fc9f3a1a9e2a70db2f
Daniel Moore <hi@danielmoore.us>
2025-01-23 18:01:05 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -6812,7 +6812,7 @@ const songs = [
 "ABC|Tears Are Not Enough|1981|pYEdXJXsipE|ABC (band)|S522|New Romantic;New Wave;Rock|",
 "Morrissey|Interlude|1994|LSpsc67Zm-I|Siouxsie Sioux|S7608|Chamber Pop;Baroque Pop;Indie Pop;Pop|",
 "Rachid Taha|Ya Rayah|1997|uUDKfK3R42Q|Rachid Taha|S11993|Algerian Chaabi;African Music;Arabic Music;North African Music;Regional Music|Ya Rayah",
-"The Sanford/Townsend Band|Smoke from a Distant Fire|1976|wCWVuCCWqzQ|Sanford-Townsend Band|S13349|Yacht Rock;Pop;Pop Rock;Rock|Smoke from a Distant Fire",
+"The Sanford/Townsend Band|Smoke from a Distant Fire|1976|IbOXharkzzE|Sanford-Townsend Band|S13349|Yacht Rock;Pop;Pop Rock;Rock|Smoke from a Distant Fire",
 "The Lovin' Spoonful|Darling Be Home Soon|1967|gr4n-nSicnM|The Lovin' Spoonful|S2284|Country Rock;Folk Rock;Rock|Darling Be Home Soon",
 "Billie Holiday|He's Funny That Way|1937|XTxmouM5OWk|Billie Holiday|S13157|Vocal Jazz;Jazz|She's Funny That Way",
 "Peter Sarstedt|Where Do You Go To (My Lovely)|1969|L8XQZYIiNgo|Peter Sarstedt|S7948|Folk Pop;Singer/Songwriter;Contemporary Folk;Folk;Pop|Where Do You Go To (My Lovely)%3F",
