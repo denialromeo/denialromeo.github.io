8d8f9d1c93e66024343c7a8613132303ae07cc19
Daniel Moore <hi@danielmoore.us>
2025-01-25 14:50:28 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -1026,7 +1026,7 @@ const songs = [
 "Madonna|Music|2000|Sdz2oW0NMFk|Madonna (entertainer)|S1381|Dance-Pop;Electropop;Dance;Electronic;Pop|Music (Madonna song)",
 "The Turtles|Happy Together|1967|h3x56WQlG_0|The Turtles|S3915|Psychedelic Pop;Sunshine Pop;Pop|Happy Together (song)",
 "Robyn|Honey|2018|Mru9GG3ur9U|Robyn|S16522|Dance-Pop;Electropop;Progressive House;Dance;Electronic;Electronic Dance Music;House;Pop|Honey (Robyn song)",
-"Buddy Holly & The Crickets|Not Fade Away|1957|m7iPbqNla6Q|The Crickets|S1257|Rockabilly;Rock;Rock & Roll|Not Fade Away (song)",
+"Buddy Holly & The Crickets|Not Fade Away|1957|0SxlGExhCf0|The Crickets|S1257|Rockabilly;Rock;Rock & Roll|Not Fade Away (song)",
 "Katy Perry|Teenage Dream|2010|98WtmW-lfeE|Katy Perry|S7069|Dance-Pop;Electropop;Dance;Electronic;Pop|Teenage Dream (Katy Perry song)",
 "Danny and the Juniors|At the Hop|1957|U3KjDpvmtwU|Danny & the Juniors|S1250|Doo-Wop;Rock & Roll;Teen Pop;Pop;R&B;Rock;Vocal Group|At the Hop",
 "Hank Williams|Your Cheatin' Heart|1953|Jg2oR91_r5I|Hank Williams|S1294|Honky Tonk;Country|Your Cheatin' Heart",
