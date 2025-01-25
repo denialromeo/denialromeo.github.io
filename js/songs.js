869660eda70980258c026ca4ebabd417caa7830a
Daniel Moore <hi@danielmoore.us>
2025-01-25 14:17:02 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -2514,7 +2514,7 @@ const songs = [
 "Blondie|Hanging on the Telephone|1978|uWhkbDMISl8|Debbie Harry|S5505|New Wave;Pop Rock;Pop;Rock|Hanging on the Telephone",
 "The Stone Roses|Made of Stone|1989|NpMzUa4YXR8|The Stone Roses|S264|Baggy / Madchester;Jangle Pop;Alternative Rock;Pop;Pop Rock;Rock|",
 "Ernest Tubb|Walking the Floor Over You|1941|0graOj19ti0|Ernest Tubb|S2752|Honky Tonk;Country|Walking the Floor Over You",
-"Billie Holiday|The Man I Love|1940|3PGq71twUD4|Billie Holiday|S5449|Standards;Vocal Jazz;Jazz;Pop;Traditional Pop|",
+"Billie Holiday|The Man I Love|1940|3PGq71twUD4|Billie Holiday|S5449|Standards;Vocal Jazz;Jazz;Pop;Traditional Pop|The Man I Love (song)",
 "Chumbawamba|Tubthumping|1997|2H5uWRjFsGc|Chumbawamba|S9|Pop Rock;Pop;Rock|Tubthumping",
 "Al Stewart|Year of the Cat|1976|tqW4xIr7nj0|Al Stewart|S5133|Singer/Songwriter;Soft Rock;Pop;Pop Rock;Rock|Year of the Cat (song)",
 "Shamir|On the Regular|2014|Lp9GgdCgMXk|Shamir (musician)|S9590|Hip House;Dance;Electronic;Electronic Dance Music;Hip Hop;House|",
