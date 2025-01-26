7e8050bd23119fe152453d94fe069ac114907cb9
Daniel Moore <hi@danielmoore.us>
2025-01-25 23:15:52 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -8936,7 +8936,7 @@ const songs = [
 "KC and the Sunshine Band|Keep It Comin' Love|1976|dc_pjiCJl3s|KC and the Sunshine Band|S12881|Disco;Dance|",
 "The Beach Boys|Little Deuce Coupe|1963|5qRW1MBvCjk|The Beach Boys|S5339|Vocal Surf;Pop;Pop Rock;Rock;Surf Music|Little Deuce Coupe (song)",
 "John Lee Hooker|Dimples|1956|oohisfAUUF0|John Lee Hooker|S6950|Electric Blues;Blues|Dimples (song)",
-"Trovadores Regionales|Gregorio Cortez|1929|oohwn77UbdM|https://www.discogs.com/artist/2639426-Trovadores-Regionales|S9383|Corrido;Hispanic Music;Latin American Music;Mexican Music;Regional Music|",
+"Trovadores Regionales|Gregorio Cortez|1929|mUgMMGvlt2s|https://www.discogs.com/artist/2639426-Trovadores-Regionales|S9383|Corrido;Hispanic Music;Latin American Music;Mexican Music;Regional Music|",
 "Al Jolson|Anniversary Song|1946|3VcQVNw2w78|Al Jolson|S5132|Traditional Pop;Pop|https://en.wikipedia.org/wiki/Waves_of_the_Danube#%22The_Anniversary_Song%22",
 "A$AP Rocky|Goldie|2012|fLCf-URqIf0|ASAP Rocky|S4994|East Coast Hip Hop;Hip Hop|Goldie (song)",
 "Steely Dan|Dirty Work|1972|kR5Ki6jjPaY|Steely Dan|S8553|Pop Rock;Pop;Rock|Dirty Work (Steely Dan song)",
