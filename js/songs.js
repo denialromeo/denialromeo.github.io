cde6c18a61721f9078ccc91a481976de516d2e2c
Daniel Moore <hi@danielmoore.us>
2025-01-29 11:53:21 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -9557,7 +9557,7 @@ const songs = [
 "Third World|96 Degrees in the Shade|1977|-V2oE8wARWo|Third World (band)|S15126|Roots Reggae;Caribbean Music;Jamaican Music;Reggae;Regional Music|",
 "Blues Image|Ride Captain Ride|1970|HqFFjoRnltg?start=6|Blues Image|S5521|Pop Rock;Pop;Rock|Ride Captain Ride",
 "Future Islands|Ran|2017|MlQunle406U|Future Islands|S16384|Synthpop;Electronic;Pop|",
-"Todd Rundgren|International Feel|1973|-1A5WBf1EFQ|Todd Rundgren|S12839|Art Pop;Progressive Pop;Pop|",
+"Todd Rundgren|International Feel|1973|Pnp2qpGgLcg|Todd Rundgren|S12839|Art Pop;Progressive Pop;Pop|",
 "Pablo Milanés|Para Vivir|1976|4RtXrSxlXkE|Pablo Milanés|S13126|Nueva trova;Singer/Songwriter;Contemporary Folk;Cuban Music;Folk;Hispanic Music;Latin American Music;Nueva canción;Nueva canción latinoamericana;Regional Music|",
 "Eddy Mitchell|Couleur menthe à l'eau|1980|W43JaufnEIU|Eddy Mitchell|S10681|Chanson;Regional Music|",
 "Mad Season|River of Deceit|1995|Fm72DPJCX58|Mad Season (band)|S14705|Alternative Rock;Rock|River of Deceit",
