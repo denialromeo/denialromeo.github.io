ca456b8727ad208b7a3b830aee5b80879c2ea840
Daniel Moore <hi@danielmoore.us>
2025-01-31 10:19:23 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -9393,7 +9393,7 @@ const songs = [
 "Hot Chip|Hungry Child|2019|rOnMZALLQPk|Hot Chip|S16792|Deep House;Garage House;Dance;Electronic;Electronic Dance Music;House|Hungry Child",
 "Squeeze|Pulling Mussels (From the Shell)|1980|9rYhXQIs66U|Squeeze (band)|S3849|New Wave;Power Pop;Pop;Pop Rock;Rock|Pulling Mussels (from the Shell)",
 "Public Enemy|Sophisticated Bitch|1987|N1N1GCUwk9s|Public Enemy (band)|S12722|East Coast Hip Hop;Hardcore Hip Hop;Hip Hop|",
-"Marvin Gaye|Trouble Man|1972|BwDT_rih0mo|Marvin Gaye|S11559|Film Soundtrack;Smooth Soul;R&B;Soul;Soundtracks|",
+"Marvin Gaye|Trouble Man|1972|vY81s1d3_L4|Marvin Gaye|S11559|Film Soundtrack;Smooth Soul;R&B;Soul;Soundtracks|",
 "The Flying Burrito Bros|Dark End of the Street|1969|2VYxdqt4k3A|The Flying Burrito Brothers|S14407|Country Rock;Country Soul;R&B;Rock;Soul|",
 "Daniel Darc|Je me souviens, je me rappelle|2004|MHUFwMA9TDo|Daniel Darc|S10496|Nouvelle chanson française;Chanson;Regional Music|",
 "Yardbirds|Still I'm Sad|1965|ONVXNH7D4Qw|The Yardbirds|S9082|Blues Rock;British Rhythm & Blues;Garage Rock;R&B;Rhythm & Blues;Rock|",
