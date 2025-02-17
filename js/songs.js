224d27db57f2180e299e58d950dc9abbf0211f41
Daniel Moore <hi@danielmoore.us>
2025-02-17 11:29:25 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -1860,7 +1860,7 @@ const songs = [
 "Pete Rock and C.L. Smooth|They Reminisce Over You (T.R.O.Y.)|1992|k6mdRv0ZdR8|Pete Rock|S169|Conscious Hip Hop;East Coast Hip Hop;Jazz Rap;Hip Hop|",
 "Grateful Dead|Box of Rain|1970|nxjvo4BRf-Y|Grateful Dead|S2554|Country Rock;Folk Rock;Roots Rock;Rock|Box of Rain",
 "The Big Pink|Dominos|2009|OGnNlQ-KNv4|The Big Pink|S4825|Alternative Dance;Indie Rock;Indietronica;Alternative Rock;Dance;Electronic;Rock|",
-"Bee Gees|How Deep Is Your Love|1977|XpqqjU7u5Yc|Bee Gees|S3203|Blue Eyed Soul;Film Soundtrack;Soft Rock;Pop;Pop Rock;Rock;Soundtracks|",
+"Bee Gees|How Deep Is Your Love|1977|XpqqjU7u5Yc|Bee Gees|S3203|Blue Eyed Soul;Film Soundtrack;Soft Rock;Pop;Pop Rock;Rock;Soundtracks|How Deep Is Your Love (Bee Gees song)",
 "Damian Marley|Welcome to Jamrock|2005|_GZlJGERbvE|Damian Marley|S3021|Dancehall;Reggae;Caribbean Music;Jamaican Music;Regional Music|Welcome to Jamrock (song)",
 "St. Vincent|Cruel|2011|Itt0rALeHE8|St. Vincent (musician)|S4985|Art Pop;Indie Pop;Pop|Cruel Summer (Taylor Swift song)",
 "The Cult|She Sells Sanctuary|1985|ZCOSPtyZAPA|The Cult|S1928|Alternative Rock;Gothic Rock;Post-Punk;Rock|She Sells Sanctuary",
