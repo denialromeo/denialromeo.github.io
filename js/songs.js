8aa034f65d36044e627f43c3af43e6eac8a88697
Daniel Moore <hi@danielmoore.us>
2025-02-24 15:36:49 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -8007,7 +8007,7 @@ const songs = [
 "Elvis Presley|Long Black Limousine|1969|_kK18OoubTg|Elvis Presley|S14359|Blue Eyed Soul;Country Soul;Pop;R&B;Soul|Long Black Limousine",
 "Echo and the Bunnymen|Never Stop|1983|BQ3k_hDrj0Q|Echo & the Bunnymen|S1893|Alternative Rock;Post-Punk;Rock|",
 "Jack White|I'm Shakin'|2012|kHMZuWgNpkw|Jack White|S11102|Blues Rock;Garage Rock;Rock|",
-"Bob Dylan|When the Ship Comes In|1964|16OLccSX2pM|Bob Dylan|S5540|Contemporary Folk;Singer/Songwriter;Folk|When the Ship Comes In",
+"Bob Dylan|When the Ship Comes In|1964|5c9_XkYYjTU|Bob Dylan|S5540|Contemporary Folk;Singer/Songwriter;Folk|When the Ship Comes In",
 "Pino Donaggio|Io che non vivo (Senza te)|1965|1snqDt7P2Lg|Pino Donaggio|S7964|Italo Pop;Pop|https://it.wikipedia.org/wiki/Io_che_non_vivo_(senza_te)",
 "Audion|Mouth to Mouth|2006|l8o9gWbhFEQ|Matthew Dear|S14087|Minimal Techno;Dance;Electronic;Electronic Dance Music;Techno|",
 "Barry Ryan|Eloise|1968|E9md5oW00q4|Barry Ryan (singer)|S5329|Baroque Pop;Pop|Eloise (Paul Ryan song)",
