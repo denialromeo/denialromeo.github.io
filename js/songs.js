4822439a378102f6fb562a000f509adcc83f4a2a
Daniel Moore <hi@danielmoore.us>
2025-02-27 12:33:03 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -1976,7 +1976,7 @@ const songs = [
 "Huey \"Piano\" Smith & The Clowns|Rockin' Pneumonia & the Boogie Woogie Flu|1957|-WWlwgoAxPE|Huey \"Piano\" Smith|S1248|New Orleans R&B;R&B|Rockin' Pneumonia and the Boogie Woogie Flu",
 "Bob Dylan|Idiot Wind|1975|Ex05XUddWMk|Bob Dylan|S2994|Folk Rock;Singer/Songwriter;Rock|Idiot Wind",
 "The Clash|The Guns of Brixton|1979|6eneRB5z1XM|The Clash|S4558|Post-Punk;Reggae;Caribbean Music;Jamaican Music;Regional Music;Rock|The Guns of Brixton",
-"Phil Phillips with The Twilights|Sea of Love|1959|lrkNRcyvtF4|Phil Phillips|S1190|Doo-Wop;R&B;Vocal Group|Sea of Love (Phil Phillips song)",
+"Phil Phillips with The Twilights|Sea of Love|1959|G9_oASbbiPQ|Phil Phillips|S1190|Doo-Wop;R&B;Vocal Group|Sea of Love (Phil Phillips song)",
 "The Stanley Brothers|Rank Strangers|1960|I16YOPmmZbs|The Stanley Brothers|S1161|Bluegrass Gospel;Close Harmony;American Folk Music;Bluegrass;Country;Country Gospel;Folk;Gospel;Regional Music;Traditional Country;Traditional Folk Music|",
 "Eddie Floyd|Knock on Wood|1966|dDloXGfNGus|Eddie Floyd|S1435|Southern Soul;R&B;Soul|Knock on Wood (Eddie Floyd song)",
 "Dizzy Gillespie|A Night in Tunisia|1946|gfLVVHxk4IM|Dizzy Gillespie|S2686|Bebop;Jazz|",
