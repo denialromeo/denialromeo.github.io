55a8b3db5170b2afcf2f75937ac76077b25c2c12
Daniel Moore <hi@danielmoore.us>
2025-03-10 00:24:16 -0700

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -8142,7 +8142,7 @@ const songs = [
 "Australian Crawl|Reckless (Don't You Be So...)|1983|JIrUqsB-0vw|Australian Crawl|S5275|Pop Rock;Pop;Rock|Reckless (Australian Crawl song)",
 "Caparezza|Fuori dal tunnel|2003|jx8GhXm-HcA|Caparezza|S12935|Comedy Rap;Pop Rap;Comedy;Hip Hop;Musical Comedy|",
 "Wilco|War on War|2002|v4rKGPi-r_I|Wilco|S9014|Alt-Country;Alternative Rock;Country;Rock|",
-"Major Lance|Um Um Um Um Um Um Um|1963|g02WmLzozs8|Major Lance|S7385|Chicago Soul;Pop Soul;Pop;R&B;Soul|Um, Um, Um, Um, Um, Um",
+"Major Lance|Um, Um, Um, Um, Um, Um|1963|u1ZLKNIvApg|Major Lance|S7385|Chicago Soul;Pop Soul;Pop;R&B;Soul|Um, Um, Um, Um, Um, Um",
 "Al Stewart|Time Passages|1978|Q2g0hrrKLNw|Al Stewart|S12977|Singer/Songwriter;Soft Rock;Pop;Pop Rock;Rock|Time Passages (song)",
 "Madness|Night Boat to Cairo|1979|lLLL1KxpYMA|Madness (band)|S14708|2 Tone;Ska|Night Boat to Cairo",
 "Timi Yuro|What's a Matter Baby (Is It Hurting You?)|1962|SCyjj9yGaOg|Timi Yuro|S8764|Pop Soul;Pop;R&B;Soul|",
