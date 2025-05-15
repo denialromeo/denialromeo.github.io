ef3c4666c9cf3a824a90b740d2a83466c1269ccf
Daniel Moore <hi@danielmoore.us>
2025-05-14 20:17:38 -0700

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -2565,7 +2565,7 @@ const songs = [
 "Spoon|Do You|2014|kLxV_FtZSt8|Spoon (band)|S9561|Indie Pop;Indie Rock;Alternative Rock;Pop;Rock|They Want My Soul",
 "Michael Jackson|Human Nature|1982|LzmzYHQSJts|Michael Jackson|S7535|Contemporary R&B;Pop Soul;Pop;R&B;Soul|Human Nature (Michael Jackson song)",
 "Band Aid|Do They Know It's Christmas?|1984|RH-xd5bPKTA|Band Aid (band)|S3166|Christmas Music;Pop;Holiday Music|Do They Know It's Christmas%3F",
-"Aretha Franklin|Do Right Woman - Do Right Man|1967|N1MW6xXjW8g?start=22|Aretha Franklin|S2514|Soul;R&B|Do Right Woman, Do Right Man",
+"Aretha Franklin|Do Right Woman, Do Right Man|1967|N1MW6xXjW8g?start=22|Aretha Franklin|S2514|Soul;R&B|Do Right Woman, Do Right Man",
 "Bon Iver|Hey, Ma|2019|HDAKS18Gv1U|Bon Iver|S16745|Art Pop;Pop|Hey, Ma (Bon Iver song)",
 "John Cage|4'33\"|1952|JTEFKFiXSx4|John Cage|S2768|Indeterminacy;Classical Music;Experimental;Modern Classical|4′33″",
 "Fleetwood Mac|Black Magic Woman|1968|dETa5maSEcw|Fleetwood Mac|S4114|Blues Rock;Psychedelic Rock;Rock|Black Magic Woman",
