7e34f88dba026cf698eceb24a2e6b5da5c7b0152
Daniel Moore <hi@danielmoore.us>
2025-07-17 13:33:33 -0700

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -6054,7 +6054,7 @@ const songs = [
 "Bob B. Soxx & the Blue Jeans|Why Do Lovers Break Each Other's Heart|1963|M_hDscmvRQY|Bob B. Soxx & the Blue Jeans|S3835|Brill Building;Girl Group;Teen Pop;Pop|Why Do Lovers Break Each Other's Heart",
 "First Aid Kit|My Silver Lining|2014|DKL4X0PZz7M|First Aid Kit (band)|S9617|Alt-Country;Folk Pop;Indie Folk;Contemporary Folk;Country;Folk;Pop|My Silver Lining (First Aid Kit song)",
 "David Axelrod|The Human Abstract|1969|l5q1SCxn-zs|David Axelrod (musician)|S5959|Baroque Pop;Psychedelic Pop;Pop|The Human Abstract",
-"Elton John|Someone Saved My Life Tonight|1975|2TfuRv4Wa_g|Elton John|S6241|Piano Rock;Pop;Pop Rock;Rock|Someone Saved My Life Tonight",
+"Elton John|Someone Saved My Life Tonight|1975|A-2vN3YJ29k|Elton John|S6241|Piano Rock;Pop;Pop Rock;Rock|Someone Saved My Life Tonight",
 "Glenn Miller|Tuxedo Junction|1940|yZ1TEqpMgZs|Glenn Miller|S6581|Big Band;Swing;Jazz|Tuxedo Junction",
 "Throbbing Gristle|United|1978|5XpqCxJZdGs|Throbbing Gristle|S8752|Industrial;Minimal Synth;Electronic;Experimental;Industrial Music;Minimal Wave|United/Zyklon B Zombie",
 "Badfinger|Day After Day|1971|HDXgApToUSQ|Badfinger|S5302|Power Pop;Pop;Pop Rock;Rock|Day After Day (Badfinger song)",
