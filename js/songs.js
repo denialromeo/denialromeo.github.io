738ee48646c7c4f0354e61e7d2a498f22ec5a826
Daniel Moore <hi@danielmoore.us>
2025-11-28 03:53:10 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -2779,7 +2779,7 @@ const songs = [
 "Jelly Roll Morton|King Porter Stomp|1924|EMXq_qGY1KI|Jelly Roll Morton|S2725|Stride;American Folk Music;Folk;Jazz;Ragtime;Regional Music;Traditional Folk Music|",
 "LCD Soundsystem|Call the Police|2017|igyk9YN6aVM|LCD Soundsystem|S16429|Post-Punk Revival;Alternative Rock;Indie Rock;Post-Punk;Rock|Call the Police (LCD Soundsystem song)",
 "The Cribs|Men's Needs|2007|UeSDJy9hYzE|The Cribs|S4342|Post-Punk Revival;Alternative Rock;Indie Rock;Post-Punk;Rock|Men's Needs",
-"The Black Keys|Tighten Up|2010|mpaPBCBjSVc|The Black Keys|S4927|Blues Rock;Rock|Tighten Up (The Black Keys song)",
+"The Black Keys|Tighten Up|2010|kH0c71-PMiY|The Black Keys|S4927|Blues Rock;Rock|Tighten Up (The Black Keys song)",
 "Elvis Presley|It's Now or Never|1960|Uwelrtb8Oho|Elvis Presley|S6258|Pop;Rhythm & Blues;R&B|It's Now or Never (song)",
 "Divinyls|I Touch Myself|1991|wv-34w8kGPM|Divinyls|S3337|Pop Rock;Pop;Rock|I Touch Myself",
 "The Andrews Sisters|Bei mir bist du schön|1937|HkEEhy33D3s|The Andrews Sisters|S5193|Swing;Traditional Pop;Vocal Group;Jazz;Pop|Bei Mir Bistu Shein",
