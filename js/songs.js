4fa1b85e3a9b215b330aab331b0c21971ca9a14d
Daniel Moore <hi@danielmoore.us>
2025-12-05 17:06:42 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -4352,7 +4352,7 @@ const songs = [
 "Ted Nugent|Cat Scratch Fever|1977|I47floRRAFs|Ted Nugent|S8699|Boogie Rock;Hard Rock;Blues Rock;Rock|Cat Scratch Fever (song)",
 "Santana|Evil Ways|1969|gVPUAntzOl4|Carlos Santana|S3794|Latin Rock;Hispanic Music;Latin American Music;Regional Music;Rock|Evil Ways",
 "Jerry Butler|He Will Break Your Heart|1960|fbcgqyOQ0RE|Jerry Butler|S1164|Rhythm & Blues;Soul;R&B|He Will Break Your Heart",
-"Cesária Évora|Sodade|1992|qx4z9V2hydQ|Cesária Évora|S14221|Morna;African Music;Cape Verdean Music;Regional Music|",
+"Cesária Évora|Sodade|1992|VVWdTKPaUdg|Cesária Évora|S14221|Morna;African Music;Cape Verdean Music;Regional Music|",
 "Nick Cave|Dig, Lazarus, Dig!!!|2008|PxWS1ufr5xI|Nick Cave|S4738|Alternative Rock;Rock|Dig, Lazarus, Dig!!! (song)",
 "Kaiser Chiefs|Ruby|2007|qObzgUfCl28|Kaiser Chiefs|S4331|Indie Rock;Pop Rock;Alternative Rock;Pop;Rock|Ruby (Kaiser Chiefs song)",
 "Weezer|Hash Pipe|2001|_9BGLtqqkVI|Weezer|S2216|Alternative Rock;Power Pop;Pop;Pop Rock;Rock|Hash Pipe",
