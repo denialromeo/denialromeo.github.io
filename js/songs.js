b5242f430e0eb970ffd7d96f26ed8e02e67557d2
Daniel Moore <hi@danielmoore.us>
2025-12-19 14:49:47 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -1684,7 +1684,7 @@ const songs = [
 "John Mellencamp|Jack & Diane|1982|h04CH9YZcpI|John Mellencamp|S1889|Heartland Rock;Pop Rock;Pop;Rock|Jack & Diane",
 "Tom Petty|Refugee|1979|fFnOfpIJL0M|Tom Petty|S1844|Heartland Rock;Rock|Refugee (Tom Petty and the Heartbreakers song)",
 "Brian Eno|1/1|1978|LKZ3fGR2SDY|Brian Eno|S4554|Ambient|",
-"X|Los Angeles|1980|yOz2PVCdKf0|X (American band)|S2602|Punk Rock;Rock|",
+"X|Los Angeles|1980|ZKp1TjP__rg|X (American band)|S2602|Punk Rock;Rock|",
 "Backstreet Boys|I Want It That Way|1999|4fndeDfaWCg|Backstreet Boys|S1373|Boy Band;Teen Pop;Pop|I Want It That Way",
 "The Futureheads|Hounds of Love|2004|PbJmqIVHH8I|The Futureheads|S3010|Post-Punk Revival;Alternative Rock;Indie Rock;Post-Punk;Rock|Hounds of Love (song)",
 "Roy Orbison|Running Scared|1961|GePCrb8W13o|Roy Orbison|S1118|Pop|Running Scared (Roy Orbison song)",
