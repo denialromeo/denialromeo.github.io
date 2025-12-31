1ca6f7f0e57ab0606066e3f8ba7e98946839875d
Daniel Moore <hi@danielmoore.us>
2025-12-30 20:40:15 -0800

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -6541,7 +6541,7 @@ const songs = [
 "Judy Garland|The Trolley Song|1944|hwP6kNIDg30|Judy Garland|S14609|Film Soundtrack;Traditional Pop;Pop;Soundtracks|The Trolley Song",
 "Barbara|La Solitude|1965|GlVrWsEUFGY|Barbara (singer)|S5318|Chanson à texte;Chanson;Regional Music;Singer/Songwriter|",
 "King Oliver's Creole Jazz Band|Canal Street Blues|1923|Y9-BuXOQbsw|King Oliver|S13221|Dixieland;Jazz|",
-"Della Reese|Don't You Know|1959|pmdFvB4yujw|Della Reese|S6026|Soul Jazz;Vocal Jazz;Jazz|",
+"Della Reese|Don't You Know?|1959|pmdFvB4yujw|Della Reese|S6026|Soul Jazz;Vocal Jazz;Jazz|Don't You Know%3F",
 "The Stone Roses|Love Spreads|1994|ct-qa6SjRZo|The Stone Roses|S2092|Alternative Rock;Neo-Psychedelia;Psychedelic Rock;Rock|Love Spreads",
 "George Harrison|Awaiting on You All|1970|_ivqxFB31I8|George Harrison|S6530|Pop Rock;Pop;Rock|Awaiting on You All",
 "Sufjan Stevens|Come On! Feel the Illinoise!|2005|nA0UOiVM1u0|Sufjan Stevens|S12344|Chamber Pop;Progressive Pop;Baroque Pop;Indie Pop;Pop|",
