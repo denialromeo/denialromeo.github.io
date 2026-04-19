9f5504f923b0c5435f15c1c28de010280444552e
Daniel Moore <hi@danielmoore.us>
2026-04-19 11:17:49 -0700

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -4664,7 +4664,7 @@ const songs = [
 "King Sunny Adé|365 Is My Number/The Message|1982|DGxgUAkFl0k|King Sunny Adé|S7110|Jùjú;African Music;Regional Music;West African Music;Yoruba Music|",
 "Rodriguez|Sugar Man|1970|esG4gK-pieA|Sixto Rodriguez|S8203|Folk Rock;Psychedelic Folk;Singer/Songwriter;Contemporary Folk;Folk;Rock|Sugar Man (song)",
 "Bonnie Raitt|Something to Talk About|1991|mJ58TVYNFro|Bonnie Raitt|S2030|Adult Contemporary;Country Pop;Pop Rock;Country;Pop;Rock|Something to Talk About (Bonnie Raitt song)",
-"Charles Brown|Black Night|1951|-71jfEwX-xQ|Charles Brown (musician)|S3242|Electric Blues;Piano Blues;Blues|Black Night (Charles Brown song)",
+"Charles Brown|Black Night|1951|-iwKH86SwdM|Charles Brown (musician)|S3242|Electric Blues;Piano Blues;Blues|Black Night (Charles Brown song)",
 "Ride|Leave Them All Behind|1992|E2jRRXou_D4|Ride (band)|S9333|Noise Pop;Shoegaze;Alternative Rock;Rock|Leave Them All Behind",
 "Tommy James and the Shondells|Hanky Panky|1964|86eJUYk-l_4|Tommy James and the Shondells|S1678|Pop Rock;Pop;Rock|Hanky Panky (Tommy James and the Shondells song)",
 "The Jayhawks|Blue|1995|Z9mJ69Tqh84|The Jayhawks|S3513|Alt-Country;Country Rock;Folk Rock;Country;Rock|",
