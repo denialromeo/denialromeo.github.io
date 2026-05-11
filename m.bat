53b3d4f4cafc21cfa9d61bb73808bb8da49e8a2d
Daniel Moore <hi@danielmoore.us>
2026-05-11 13:17:39 -0700

    Commit.

--- m.bat
+++ m.bat
@@ -1,2 +1,20 @@
-@make %*
-exit
+@echo off
+
+git pull origin master
+
+@REM jekyll build
+
+git add --all .
+git commit --allow-empty -m "Commit."
+git push origin master --force
+
+cd _site
+
+git pull origin master
+git add --all .
+git commit --allow-empty -m "Commit."
+git push origin master
+
+del /s /q *~
+
+pause
