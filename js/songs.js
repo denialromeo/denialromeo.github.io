9f523c352d0196ac9e81864e2142fa392f4e4374
Daniel Moore <hi@danielmoore.us>
2026-07-13 11:17:29 -0700

    Commit.

--- js/songs.js
+++ js/songs.js
@@ -7942,7 +7942,7 @@ const songs = [
 "E-40|Tell Me When to Go|2006|2GZbaXdK8Js|E-40|S4034|Hyphy;West Coast Hip Hop;Hip Hop|Tell Me When to Go",
 "Maroon 5|Makes Me Wonder|2007|sAebYQgy4n4|Maroon 5|S7439|Funktronica;Pop Rock;Dance;Electronic;Electronic Dance Music;Pop;Rock|Makes Me Wonder",
 "Erroll Garner|I'll Remember April|1956|I_aILGaLqyc|Erroll Garner|S10756|Bebop;Stride;American Folk Music;Folk;Jazz;Ragtime;Regional Music;Traditional Folk Music|I'll Remember April (song)",
-"Brass Construction|Movin'|1975|VzjxvopUSzE|Brass Construction|S12854|Disco;Funk;Dance;R&B|Movin' (Brass Construction song)",
+"Brass Construction|Movin'|1975|9eVyQikja8I|Brass Construction|S12854|Disco;Funk;Dance;R&B|Movin' (Brass Construction song)",
 "Stone Temple Pilots|Trippin' on a Hole in a Paper Heart|1996|HVPzWkdhwrw|Stone Temple Pilots|S12330|Grunge;Power Pop;Alternative Rock;Pop;Pop Rock;Rock|Trippin' on a Hole in a Paper Heart",
 "Betty Boo|Where Are You Baby?|1990|_4iWgsGgBiY|Betty Boo|S222|Dance-Pop;Pop Rap;Dance;Hip Hop;Pop|Where Are You Baby%3F",
 "Barbra Streisand|Guilty|1980|Phh9B1-4EYs|Barbra Streisand|S5324|Adult Contemporary;Blue Eyed Soul;Pop|Guilty (Barbra Streisand and Barry Gibb song)",
