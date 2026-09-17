656903772604420d895de19998b03b20b0275d78
Daniel Moore <hi@danielmoore.us>
2026-09-17 01:46:23 -0700

    Commit.

--- js/classical.js
+++ js/classical.js
@@ -790,7 +790,7 @@ const classical = [
 "Bela Bartok|Music for Strings, Percussion, and Celesta|1936|HGJcsTtJ188|Béla Bartók|Music for Strings, Percussion and Celesta",
 "Dmitri Shostakovich|Symphony No. 4|1936|HQrhffGoy2E|Dmitri Shostakovich|Symphony No. 4 (Shostakovich)",
 "Franz Schmidt|The Book with Seven Seals|1937|gQfMFH7UU7s|Franz Schmidt (composer)|The Book with Seven Seals",
-"Benjamin Britten|Variations on a Theme of Frank Bridge|1937|_TkHXHH0w0Q|Benjamin Britten|Variations on a Theme of Frank Bridge",
+"Benjamin Britten|Variations on a Theme of Frank Bridge|1937|j0aoHyd2QBQ|Benjamin Britten|Variations on a Theme of Frank Bridge",
 "Dmitri Shostakovich|Symphony No. 5|1937|cg0M4LzEITQ?start=3|Dmitri Shostakovich|Symphony No. 5 (Shostakovich)",
 "Richard Strauss|Daphne|1937|OwQ6wmcYGiY|Richard Strauss|Daphne (opera)",
 "Dag Wiren|Serenade for Strings|1937|z4jZ4fIjSWU?start=3|Dag Wirén|",
